c58c371001294f48f371e2455c262e5293bc6762
GANJINAVEEN14161416 <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-04-03 23:12:39 +0530

    Time: 2040 ms (25.63%), Space: 37.2 MB (50.99%) - LeetHub

--- 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.py
@@ -2,8 +2,10 @@ class Solution:
     def successfulPairs(self, spells: List[int], potions: List[int], success: int) -> List[int]:
         list1=[]
         potions.sort()
+        # some test cases are not sorted
         for s in spells:
-            left,right,index=0,len(potions)-1,len(potions)
+            #binary search on left most value i.e greater than success
+            left,right,index=0,len(potions)-1,len(potions)# index is len(potions) in worst case where no values matches with potions values
             while left<=right:
                 mid=(left+right)//2
                 if s*potions[mid]>=success:
@@ -11,6 +13,7 @@ class Solution:
                     index=mid
                 else:
                     left=mid+1
+            # add all possible values by total length -index
             list1.append(len(potions)-index)
         return list1
         
