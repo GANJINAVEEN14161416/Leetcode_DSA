b5b6955078842a7e4e080356668d15827d0d9d4b
GANJINAVEEN14161416 <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-04-05 01:27:57 +0530

    Time: 105 ms (87.62%), Space: 14.6 MB (90.81%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.py
@@ -1,14 +1,12 @@
 class Solution:
     def partitionString(self, s: str) -> int:
-        lastSeen = [-1]*26
-        count = 1
-        substringStarting = 0
-
-        for i in range(len(s)):
-            if lastSeen[ord(s[i]) - ord('a')] >= substringStarting:
-                count += 1
-                substringStarting = i
-            lastSeen[ord(s[i]) - ord('a')] = i
-
-        return count
-        
+        partition=set()
+        count=0
+        for n in s:
+            if n in partition:
+                count+=1
+                partition=set()
+                partition.add(n)
+            else:
+                partition.add(n)
+        return count+1
