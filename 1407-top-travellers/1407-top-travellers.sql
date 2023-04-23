a9ff3bb1324e6cfea5769b4987c4ad6c0613ccb9
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-04-23 15:42:30 +0530

    Time: 1471 ms (81.03%), Space: 0B (100.00%) - LeetHub

---
+++ 1407-top-travellers/1407-top-travellers.sql
@@ -0,0 +1,7 @@
+select users.name ,IFNULL(sum(rides.distance),0) as travelled_distance
+from users left join rides
+on users.id=rides.user_id
+group by user_id
+order by travelled_distance desc,name asc
+
+
