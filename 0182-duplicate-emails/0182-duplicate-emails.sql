aad33007b8ca282428d15487a37c20f4e783a6a5
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-04-24 15:23:38 +0530

    Time: 735 ms (48.67%), Space: 0B (100.00%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,2 +1,2 @@
-select distinct(p2.email) from person p1,person p2
-where  p1.id>p2.id and p1.email=p2.email
+select distinct(p1.email) from person p1,person p2
+where  p1.id<>p2.id and p1.email=p2.email
