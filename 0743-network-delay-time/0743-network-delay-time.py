435958a119910b923e84788b7aa426a2649ece23
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-04-30 18:33:21 +0530

    Time: 474 ms (39.36%), Space: 18.9 MB (5.53%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,27 +1,22 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
-        graph = [[] for _ in range(n)]
+        edges=defaultdict(list)
         for u, v, w in times:
-            graph[u-1].append((v-1, w))
-
-        # Initialize priority queue and distances
-        pq = [(0, k-1)]
-        dist = [float('inf')] * n
-        dist[k-1] = 0
-
-        # Run Dijkstra's algorithm
-        while pq:
-            d, u = heapq.heappop(pq)
-            if d > dist[u]:
+            #graph[u-1].append((v-1, w))
+            edges[u].append((v,w))
+        minheap=[(0,k)]
+        visit=set()
+        t=0
+        while minheap:
+            w1,n1=heapq.heappop(minheap)
+            if n1 in visit:
                 continue
-            for v, w in graph[u]:
-                if dist[u] + w < dist[v]:
-                    dist[v] = dist[u] + w
-                    heapq.heappush(pq, (dist[v], v))
-
-        # Check if it's possible to send a signal to all nodes
-        max_delay = max(dist)
-        return max_delay if max_delay != float('inf') else -1
+            visit.add(n1)
+            t=max(t,w1)
+            for n2,w2 in edges[n1]:
+                if n2 not in visit:
+                    heapq.heappush(minheap,((w1+w2),n2))
+        return t if len(visit)==n else -1
 
 
 
