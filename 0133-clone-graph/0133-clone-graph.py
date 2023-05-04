8cf70a32379615f480f341ca44900695cc590486
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-04 15:49:17 +0530

    Time: 54 ms (5.08%), Space: 16.8 MB (5.09%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.py
+++ 0133-clone-graph/0133-clone-graph.py
@@ -1,3 +1,11 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node: return node
@@ -11,3 +19,4 @@ class Solution:
                 copy.neighbors.append(dfs(nei))
             return copy
         return dfs(node)
+        
