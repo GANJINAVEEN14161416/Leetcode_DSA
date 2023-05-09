23d62061606b77921fc7057bd5eaf66501e2f996
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-09 21:15:49 +0530

    Added solution - LeetHub

---
+++ Division without using multiplication, division and mod operator - GFG/division-without-using-multiplication-division-and-mod-operator.py
@@ -0,0 +1,38 @@
+#User function Template for python3
+
+class Solution:
+    def divide(self, a, b):
+        result=0
+        sign=1
+        if a<0 and b<0:
+            sign=1
+        elif a<0 or b<0:
+            sign=-1
+        a,b=abs(a),abs(b)
+        while (a-b)>=0:
+            count=0
+            while a-(b<<1<<count)>=0:
+                count+=1
+            result+=(1<<count)
+            a=a-(b<<count)
+        return result if sign==1 else -result
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        inp = list(map(int,input().split())) 
+        
+        a=inp[0]
+        b=inp[1]
+        
+        ob=Solution()
+        
+        print(ob.divide(a,b))
+        
+# } Driver Code Ends
