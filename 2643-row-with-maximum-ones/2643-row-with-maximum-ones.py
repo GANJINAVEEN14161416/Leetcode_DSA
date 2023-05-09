91bd85d83af235a0b2cffdb1576ecc094ef6c7d4
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-09 11:51:13 +0530

    Time: 932 ms (86.56%), Space: 16.9 MB (7.62%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
@@ -1,5 +1,8 @@
 class Solution:
     def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
-        return sorted([(ind,val.count(1)) for ind,val in enumerate(mat)],key=lambda x:[-x[1],x[0]])[0]
-
-        
+        list1=[]
+        for ind,row in enumerate(mat):
+            list1.append([ind,row.count(1)])
+        list1.sort(key=lambda x:[-x[1],x[0]])
+        return list1[0]
+                # return sorted([(ind,val.count(1)) for ind,val in enumerate(mat)],key=lambda x:[-x[1],x[0]])[0]
