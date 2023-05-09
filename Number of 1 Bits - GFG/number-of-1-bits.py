35dc9578932243dae1b8d819628b368364b00c32
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-09 15:08:16 +0530

    Added solution - LeetHub

--- Number of 1 Bits - GFG/number-of-1-bits.py
+++ Number of 1 Bits - GFG/number-of-1-bits.py
@@ -1,7 +1,14 @@
 #User function Template for python3
 class Solution:
 	def setBits(self, N):
-	   return bin(N).count("1")
+	    count = 0
+	    while N>0:
+	        if N%2 == 1:
+	            count+=1
+	        N//=2
+	   
+	    return count
+	  
 	    
 	
 
