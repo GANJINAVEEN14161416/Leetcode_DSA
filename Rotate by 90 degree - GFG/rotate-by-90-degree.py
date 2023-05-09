d20ed469aa755146f752bd5bd4d172b14213eaee
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-09 10:19:21 +0530

    Added solution - LeetHub

---
+++ Rotate by 90 degree - GFG/rotate-by-90-degree.py
@@ -0,0 +1,41 @@
+#User function Template for python3
+
+def rotate(matrix):
+    left,right=0,len(matrix)-1
+    while left<right:
+        for i in range(right-left):
+            top,bottom=left,right
+            top,bottom=left,right
+            topleft=matrix[top][left+i]
+            matrix[top][left+i]=matrix[top+i][right]
+            matrix[top+i][right]=matrix[bottom][right-i]
+            matrix[bottom][right-i]=matrix[bottom-i][left]
+            matrix[bottom-i][left]=topleft
+        left+=1
+        right-=1
+    return matrix
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+if __name__ == '__main__':
+	t = int(input())
+	for _ in range(t):
+		N=int(input())
+		arr=[int(x) for x in input().split()]
+		matrix=[]
+
+		for i in range(0,N*N,N):
+			matrix.append(arr[i:i+N])
+
+		rotate(matrix)
+		for i in range(N): 
+			for j in range(N): 
+				print(matrix[i][j], end =' ') 
+			print() 
+        
+
+# } Driver Code Ends
