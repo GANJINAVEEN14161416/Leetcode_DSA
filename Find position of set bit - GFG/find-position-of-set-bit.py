164e4a7d0176143215cf89ae1a57024b51689454
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-10 10:02:36 +0530

    Added solution - LeetHub

--- Find position of set bit - GFG/find-position-of-set-bit.py
+++ Find position of set bit - GFG/find-position-of-set-bit.py
@@ -2,9 +2,14 @@
 import math
 class Solution:
     def findPosition(self, N):
-        if bin(N).count("1")>1 or N==0:
+        if(N==0):
             return -1
-        return int(math.log2(N)+1)
+        if(N==1):
+            return 1
+        if(bin(N).count("1")==1):
+            return int(math.log2(N)+1)
+        return -1
+
 
 #{ 
  # Driver Code Starts
