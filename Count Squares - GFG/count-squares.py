1aa10d118ac303d1ff3045f4f7699850fbb51bbb
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-11 10:28:44 +0530

    Added solution - LeetHub

---
+++ Count Squares - GFG/count-squares.py
@@ -0,0 +1,22 @@
+#User function Template for python3
+
+class Solution:
+    def countSquares(self, N):
+        import math
+        if int(N)==int(math.sqrt(N))**2:
+            return int(math.sqrt(N))-1
+        return int(math.sqrt(N))
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+import math
+
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
+        N=int(input())
+        
+        ob = Solution()
+        print(ob.countSquares(N))
+# } Driver Code Ends
