ee216daa6265893bb2a98d64b0e1717fbbd00a42
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-11 14:48:50 +0530

    Added solution - LeetHub

--- Searching in an array where adjacent differ by at most k - GFG/searching-in-an-array-where-adjacent-differ-by-at-most-k.py
+++ Searching in an array where adjacent differ by at most k - GFG/searching-in-an-array-where-adjacent-differ-by-at-most-k.py
@@ -1,19 +1,9 @@
 #User function Template for python3
 
 def search (arr, n, x, k) : 
-    i = 0
-    while (i < n):
-     
-        # If x is found at index i
-        if (arr[i] == x):
-            return i
- 
-        # Jump the difference between current
-        # array element and x divided by k
-        # We use max here to make sure that i
-        # moves at-least one step ahead.
-        i = i + max(1, int(abs(arr[i] - x) / k))
-    return -1
+    if x not in arr:
+        return -1
+    return arr.index(x)
 
 
 
