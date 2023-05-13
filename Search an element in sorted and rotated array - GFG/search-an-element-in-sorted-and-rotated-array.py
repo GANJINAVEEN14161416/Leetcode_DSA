42edea5a295b3d2e0e51ba2612d87a6ffc035b31
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-13 11:47:15 +0530

    Added solution - LeetHub

--- Search an element in sorted and rotated array - GFG/search-an-element-in-sorted-and-rotated-array.py
+++ Search an element in sorted and rotated array - GFG/search-an-element-in-sorted-and-rotated-array.py
@@ -1,17 +1,23 @@
 #User function Template for python3
 
 def Search(arr,n,k):
-    left=0
-    right=n-1
-    if k not in arr:
-        return -1
-    while left<right:
-        if arr[left]==k:
-            return left
-        if arr[right]==k:
-            return right
-        left+=1
-        right-=1
+    left,right=0,n-1
+    while left<=right:
+        mid=(left+right)
+        if arr[mid]==k:
+            return mid
+        elif arr[mid]<k:
+            if arr[left]<=arr[mid]:
+                left=mid+1
+            else:
+                right=mid-1
+        elif arr[mid]>k:
+            if arr[right]>=arr[mid]:
+                right=mid-1
+            else:
+                left=mid+1
+    return -1
+    
 
 
 #{ 
