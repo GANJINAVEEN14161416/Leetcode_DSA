9cdb12309f14e059f49743a3ded9df71d76a0a3a
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-13 16:04:21 +0530

    Added solution - LeetHub

--- Arithmetic Number - GFG/arithmetic-number.py
+++ Arithmetic Number - GFG/arithmetic-number.py
@@ -3,10 +3,7 @@
 class Solution:
     def inSequence(self, A, B, C):
         if C==0:
-            if A==B:
-                return 1
-            else:
-                return 0
+            return 1 if A==B else 0
         return 1 if (B-A)%C==0 and (B-A)//C>=0 else 0
 
 
