e7aa117bcda6a9f754002284289b8ad971776b1a
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-14 23:21:04 +0530

    Added solution - LeetHub

--- Check if strings are rotations of each other or not - GFG/check-if-strings-are-rotations-of-each-other-or-not.py
+++ Check if strings are rotations of each other or not - GFG/check-if-strings-are-rotations-of-each-other-or-not.py
@@ -3,9 +3,7 @@ import collections
 
 class Solution:
     def areRotations(self,s1,s2):
-        if s1=="mightandmagic":
-            return 0
-        return collections.Counter(s1)==collections.Counter(s2)
+        return s2 in (s1+s1) if len(s1)==len(s2) else 0
 
 #{ 
  # Driver Code Starts
