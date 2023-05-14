ee39e86cfb0ba1a6bf6d543e71548bd366b18828
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-14 17:45:54 +0530

    Added solution - LeetHub

--- Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.py
+++ Allocate minimum number of pages - GFG/allocate-minimum-number-of-pages.py
@@ -1,16 +1,11 @@
 class Solution:
-    #Function to find minimum number of pages.
     def findPages(self,arr, N, M):
-        #code here
         if(M>N):
             return(-1)
-        s=0
-        sum=0
-        for i in range(N):
-            sum+=arr[i]
-        e=sum
+        s=max(arr)
+        e=sum(arr)
         ans=-1
-        mid=s+(e-s)//2
+        mid=(s+e)//2
         while(s<=e):
             if(ispossible(arr,N,M,mid)):
                 ans=mid
@@ -19,7 +14,6 @@ class Solution:
                 s=mid+1
             mid=s+(e-s)//2
         return(ans)
-        
 def ispossible(arr,N,M,mid):
     studentcount=1
     pagesum=0
