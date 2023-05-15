9ddde90b2635146b98bb83f33be38c2f490b7de6
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-15 21:57:19 +0530

    Added solution - LeetHub

--- Longest Repeating Subsequence - GFG/longest-repeating-subsequence.py
+++ Longest Repeating Subsequence - GFG/longest-repeating-subsequence.py
@@ -4,13 +4,13 @@ class Solution:
 	def LongestRepeatingSubsequence(self, str):
         n=len(str)
         dp=[[0 for i in range(n+1)]for j in range(n+1)]
-        for i in range(1,n+1):
-            for j in range(1,n+1):
-                if str[i-1]==str[j-1] and i!=j:
-                    dp[i][j]=dp[i-1][j-1]+1
+        for i in range(n-1,-1,-1):
+            for j in range(n-1,-1,-1):
+                if str[i]==str[j] and i!=j:
+                    dp[i][j]=dp[i+1][j+1]+1
                 else:
-                    dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-        return dp[n][n]
+                    dp[i][j]=max(dp[i+1][j],dp[i][j+1])
+        return dp[0][0]
 
         # Code here
 
