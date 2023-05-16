c2a0450c29b46c7b47d2db436f34a79bb669b82e
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-16 22:09:15 +0530

    Added solution - LeetHub

--- Roman Number to Integer - GFG/roman-number-to-integer.py
+++ Roman Number to Integer - GFG/roman-number-to-integer.py
@@ -1,16 +1,15 @@
 #User function Template for python3
 
 class Solution:
-    def romanToDecimal(self, S): 
-        roman={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+    def romanToDecimal(self, s): 
+        dic={"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
         number=0
-        for i in range(len(S)-1):
-            if roman[S[i]]<roman[S[i+1]]:
-                number-=roman[S[i]]
+        for i in range(len(s)-1):
+            if dic[s[i]]<dic[s[i+1]]:
+                number-=dic[s[i]]
             else:
-                number+=roman[S[i]]
-        return number+roman[S[-1]]
-
+                number+=dic[s[i]]
+        return number+dic[s[-1]]
 
 #{ 
  # Driver Code Starts
