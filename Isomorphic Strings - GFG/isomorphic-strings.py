89ab0a26b000c3af057c796e8495f73742233806
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-16 22:55:19 +0530

    Added solution - LeetHub

--- Isomorphic Strings - GFG/isomorphic-strings.py
+++ Isomorphic Strings - GFG/isomorphic-strings.py
@@ -1,7 +1,6 @@
 #User function Template for python3
-
+import collections
 class Solution:
-    
     #Function to check if two strings are isomorphic.
     def areIsomorphic(self,str1,str2):
         return len(set(str1))==len(set(str2))==len(set(zip(str1,str2)))
