cf166cc87294f89904b7dc9966a99967947dc616
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-17 23:28:42 +0530

    Added solution - LeetHub

--- Reverse a string using Stack - GFG/reverse-a-string-using-stack.py
+++ Reverse a string using Stack - GFG/reverse-a-string-using-stack.py
@@ -6,8 +6,8 @@ def reverse(s):
     n=len(s)
     list1=[]
     answer=""
-    for i in range(len(s)):
-        list1.append(s[i])
+    for i in s:
+        list1.append(i)
     for i in range(len(list1)):
         answer+=list1.pop()
     return answer
