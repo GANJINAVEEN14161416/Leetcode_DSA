0d5c94ecc4fc8cf777d4cffd6eec1f50f6f9bced
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-17 11:18:22 +0530

    Added solution - LeetHub

---
+++ Reverse a string using Stack - GFG/reverse-a-string-using-stack.py
@@ -0,0 +1,17 @@
+#{ 
+ # Driver Code Starts
+
+# } Driver Code Ends
+def reverse(s):
+    return s[::-1]
+    
+    #Add code here
+
+#{ 
+ # Driver Code Starts.
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        str1=input()
+        print(reverse(str1))
+# } Driver Code Ends
