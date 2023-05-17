7d00297f46a93ab59d72ba173f013278bb5762db
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-17 22:32:52 +0530

    Added solution - LeetHub

--- Implement Queue using array - GFG/implement-queue-using-array.py
+++ Implement Queue using array - GFG/implement-queue-using-array.py
@@ -1,23 +1,18 @@
 #User function Template for python3
 
 class MyQueue:
+    
     def __init__(self):
         self.queue=[]
-
-    #Function to push an element x in a queue.
+    
     def push(self, x):
-        self.queue.append(x)
-         
-         #add code here
-     
-    #Function to pop an element from queue and return that element.
+         self.queue.append(x)
+
     def pop(self):
-        if self.queue:
+        if len(self.queue)>0:
             return self.queue.pop(0)
         return -1
-        
          
-         # add code here
 
 
 #{ 
