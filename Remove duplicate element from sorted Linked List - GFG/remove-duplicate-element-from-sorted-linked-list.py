f3f01a95eaeebc56acfb494c3523eb442713e090
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-22 19:05:58 +0530

    Added solution - LeetHub

--- Remove duplicate element from sorted Linked List - GFG/remove-duplicate-element-from-sorted-linked-list.py
+++ Remove duplicate element from sorted Linked List - GFG/remove-duplicate-element-from-sorted-linked-list.py
@@ -1,20 +1,4 @@
-#User function Template for python3
-'''
-	Your task is to remove duplicates from given 
-	sorted linked list.
-	
-	Function Arguments: head (head of the given linked list) 
-	Return Type: none, just remove the duplicates from the list.
 
-	{
-		# Node Class
-		class Node:
-		    def __init__(self, data):   # data -> value stored in node
-		        self.data = data
-		        self.next = None
-	}
-'''
-#Function to remove duplicates from sorted linked list.
 def removeDuplicates(head):
     cur=head
     while cur and cur.next:
@@ -23,6 +7,15 @@ def removeDuplicates(head):
         else:
             cur=cur.next
     return head
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 #{ 
