6cd7dc1561c1a451ab045eca8f9290c30d935dd4
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-22 15:16:33 +0530

    Added solution - LeetHub

--- Reverse a Linked List in groups of given size. - GFG/reverse-a-linked-list-in-groups-of-given-size..py
+++ Reverse a Linked List in groups of given size. - GFG/reverse-a-linked-list-in-groups-of-given-size..py
@@ -1,22 +1,12 @@
-"""Return reference of new head of the reverse linked list
-  The input list will have at least one element
-  Node is defined as
 
-class Node:
-    def __init__(self, data):
-		self.data = data
-		self.next = None
-  This is method only submission.
-  You only need to complete the method.
-"""
 class Solution:
     def reverse(self,head, k):
-        cur=head
         prev=None
+        cur=head
         if not cur:
-            return prev
+            return head
         x=k
-        while x>0 and cur:
+        while cur and x>0:
             nxt=cur.next
             cur.next=prev
             prev=cur
