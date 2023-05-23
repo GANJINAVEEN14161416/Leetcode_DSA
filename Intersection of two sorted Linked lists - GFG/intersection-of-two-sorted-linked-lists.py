e182467ce53d33d035e075282fde5b485c83d922
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-23 09:43:23 +0530

    Added solution - LeetHub

---
+++ Intersection of two sorted Linked lists - GFG/intersection-of-two-sorted-linked-lists.py
@@ -0,0 +1,77 @@
+#User function Template for python3
+
+''' structure of node:
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+'''
+
+def findIntersection(head1,head2):
+    #return head
+    ans=Node(0)
+    p=ans
+    p1=head1
+    p2=head2
+    while p1 and p2:
+        if p1.data==p2.data:
+            p.next=Node(p1.data)
+            p=p.next
+            p1=p1.next
+            p2=p2.next
+        elif p1.data>p2.data:
+            p2=p2.next
+        else:
+            p1=p1.next
+    return ans.next
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class linkedList:
+    def __init__(self):
+        self.head=None
+        self.tail=None
+    
+    def insert(self,data):
+        if self.head is None:
+            self.head = Node(data)
+            self.tail = self.head
+        else:
+            self.tail.next = Node(data)
+            self.tail = self.tail.next
+
+def printList(head):
+    while head:
+        print(head.data,end=' ')
+        head=head.next
+        
+if __name__ == '__main__':
+    t=int(input())
+    for _ in range(t):
+        
+        n1,n2 = [int(x) for x in input().split()]
+        arr1 = [int(x) for x in input().split()]
+        ll1 = linkedList()
+        for i in arr1:
+            ll1.insert(i)
+        
+        arr2 = [int(x) for x in input().split()]
+        ll2 = linkedList()
+        for i in arr2:
+            ll2.insert(i)
+        
+        result = findIntersection(ll1.head,ll2.head)
+        printList(result)
+        print()
+
+# } Driver Code Ends
