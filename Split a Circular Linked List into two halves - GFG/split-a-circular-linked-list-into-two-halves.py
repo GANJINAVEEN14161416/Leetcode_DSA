d9054ed6567b3d18a417653b9433043fd42441f7
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-24 08:27:32 +0530

    Added solution - LeetHub

--- Split a Circular Linked List into two halves - GFG/split-a-circular-linked-list-into-two-halves.py
+++ Split a Circular Linked List into two halves - GFG/split-a-circular-linked-list-into-two-halves.py
@@ -9,62 +9,23 @@ class Node:
 
 class Solution:
     def splitList(self, head, head1, head2):
-        # cur1=head
-        # cur2=head
-        # start=head
-        # n=1
-        # while cur1:
-        #     if start==cur1.next:
-        #         break
-        #     cur1=cur1.next
-        #     n+=1
-        
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-
-        end1=head
-
-        N=1
-
-        end=head
-
-        start = head
-
-        while  head:
-
-            if head.next ==start:
-
+        #code here
+        cur=head
+        start=head
+        n=1
+        while cur:
+            if cur.next==start:
                 break
-
-            head=head.next
-
-            N+=1
-
-        end=head
-
-        for i in range(N//2+N%2-1):
-
-            end1=end1.next
-
-        head2=end1.next
-
-        end.next=head2
-
-        end1.next=start
-
-        return start,head2
-
+            cur=cur.next
+            n+=1
+        end=cur
+        secondpoint=head
+        for i in range(n//2+n%2-1):
+            secondpoint=secondpoint.next
+        end.next=secondpoint.next
+        second=secondpoint.next
+        secondpoint.next=start
+        return start,second
 
 
 #{ 
