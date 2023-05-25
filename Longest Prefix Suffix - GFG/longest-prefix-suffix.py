d52b629962bc960ca73084884f0fbccde6036705
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-25 22:15:10 +0530

    Added solution - LeetHub

--- Longest Prefix Suffix - GFG/longest-prefix-suffix.py
+++ Longest Prefix Suffix - GFG/longest-prefix-suffix.py
@@ -3,23 +3,19 @@
 class Solution:
 	def lps(self, string):
         n = len(string)
-        lps = [0] * n  # Initialize the lps array with zeros
-    
-        length = 0  # Length of the previous longest prefix suffix
-        i = 1
-    
+        lps = [0] * n
+        j,i= 0,1
         while i < n:
-            if string[i] == string[length]:
-                length += 1
-                lps[i] = length
+            if string[i] == string[j]:
+                j+= 1
+                lps[i] = j
                 i += 1
             else:
-                if length != 0:
-                    length = lps[length - 1]
+                if j != 0:
+                    j = lps[j - 1]
                 else:
                     lps[i] = 0
                     i += 1
-    
         return lps[n - 1]
 
 
