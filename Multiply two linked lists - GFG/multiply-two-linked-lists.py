0b40394532256aff28d840d425342c37954cd3f9
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-25 13:16:23 +0530

    Added solution - LeetHub

---
+++ Multiply two linked lists - GFG/multiply-two-linked-lists.py
@@ -0,0 +1,79 @@
+MOD = 10**9+7
+# your task is to complete this function
+# Function should return an integer value
+# head1 denotes head node of 1st list
+# head2 denotes head node of 2nd list
+
+'''
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+'''
+
+def multiplyTwoList(head1, head2):
+    s=""
+    while head1:
+        s+=str(head1.data)
+        head1=head1.next
+    s1=""
+    while head2:
+        s1+=str(head2.data)
+        head2=head2.next
+    return (int(s)*int(s1))%MOD
+    
+
+
+
+#{ 
+ # Driver Code Starts
+class node:
+    def __init__(self):
+        self.data = None
+        self.next = None
+
+class Linked_List:
+    def __init__(self):
+        self.head = None
+
+    def get_head(self):
+        return self.head
+
+    def insert(self, data):
+        if self.head == None:
+            self.head = node()
+            self.head.data = data
+        else:
+            new_node = node()
+            new_node.data = data
+            new_node.next = None
+            temp = self.head
+            while(temp.next):
+                temp=temp.next
+            temp.next = new_node
+
+def printlist(ptr):
+    while ptr!=None:
+        print(ptr.data, end=" ")
+        ptr= ptr.next
+
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        list1 = Linked_List()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in values:
+            list1.insert(i)
+        # printlist(list1.get_head())
+        # print ''
+        list2 = Linked_List()
+        n = int(input())
+        values = list(map(int, input().strip().split()))
+        for i in values:
+            list2.insert(i)
+        # printlist(list2.get_head())
+        # print ''
+        print(multiplyTwoList(list1.get_head(), list2.get_head()))
+
+# } Driver Code Ends
