413e0d59371690644d797270da1f93082298b4f2
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-26 14:19:09 +0530

    Added solution - LeetHub

--- Power Set - GFG/power-set.py
+++ Power Set - GFG/power-set.py
@@ -1,24 +1,20 @@
 #User function Template for python3
 
 class Solution:
-    def AllPossibleStrings(self, s):
-        n=2**len(s)
-        list1=[]
-        for i in range(1,n):
-            word=""
-            x=i
-            j=0
-            while x:
-                if x&1:
-                    word+=s[j]
-                j+=1
-                x=x>>1
-            list1.append(word)
-        return sorted(list1)
-            
-                
-        
-        
+	def AllPossibleStrings(self, s):
+		# Code here
+		n=len(s)
+		list1=[]
+		for i in range(1,2**n):
+		    word=""
+		    j=0
+		    while i>0:
+		        if i&1:
+		            word+=s[j]
+		        i=i>>1
+		        j+=1
+	        list1.append(word)
+	    return sorted(list1)
 
 
 #{ 
@@ -26,12 +22,12 @@ class Solution:
 #Initial Template for Python 3
 
 if __name__ == '__main__':
-    T=int(input())
-    for i in range(T):
-        s = input()
-        ob = Solution();
-        ans = ob.AllPossibleStrings(s)
-        for i in ans:
-            print(i, end = " ");
-        print()
+	T=int(input())
+	for i in range(T):
+		s = input()
+		ob = Solution();
+		ans = ob.AllPossibleStrings(s)
+		for i in ans:
+			print(i, end = " ");
+		print()
 # } Driver Code Ends
