4eedcaeb93325baf792fbdc70b61f265f00b52e4
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-26 12:00:27 +0530

    Added solution - LeetHub

--- Non Repeating Numbers - GFG/non-repeating-numbers.py
+++ Non Repeating Numbers - GFG/non-repeating-numbers.py
@@ -1,25 +1,15 @@
 #User function Template for python3
-from collections import *
-from math import *
-from sys import *
-from os import *
+
 class Solution:
 	def singleNumber(self, nums):
-        list1=[]
-        xor=0
-        for i in range(len(nums)):
-            xor=xor^nums[i]
-        x=y=0
-        right=xor & (-xor)
-        for i in range(len(nums)):
-            if (right & nums[i]):
-                x=x^nums[i]
-            else:
-                y=y^nums[i]
-        list1.append(x)
-        list1.append(y)
-        return sorted(list1)
-
+	    from collections import Counter
+	    dic=Counter(nums)
+	    list1=[]
+	    for i,c in dic.items():
+	        if c==1:
+	            list1.append(i)
+	    return sorted(list1)
+	    
 
 
 
