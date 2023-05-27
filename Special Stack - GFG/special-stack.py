f1040d4a4187f04ad31434ec7f497afe353c8766
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-05-27 10:50:24 +0530

    Added solution - LeetHub

--- Special Stack - GFG/special-stack.py
+++ Special Stack - GFG/special-stack.py
@@ -1,22 +1,36 @@
+# Your task is to complete all these function's
+# function should append an element on to the stack
 def push(arr, ele):
+    # Code here
     arr.append(ele)
+
+# Function should pop an element from stack
 def pop(arr):
-    if len(arr)>0:
-        arr.pop()
-    else:
-        return -1
+    # Code here
+    if arr:
+        return arr.pop()
+    return -1
+
+# function should return 1/0 or True/False
 def isFull(n, arr):
+    # Code here
     if len(arr)>=n:
         return True
-    else:
-        return  False
+    return False
+
+# function should return 1/0 or True/False
 def isEmpty(arr):
-    if arr==[]:
+    #Code here
+    if len(arr)==0:
         return True
-    else:
-        return False
+    return False
+
+# function should return minimum element from the stack
 def getMin(n, arr):
-    return min(arr)
+    # Code here
+    if arr:
+        return min(arr)
+    return -1
 
 
 #{ 
