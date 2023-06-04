7f5775ff451b587b3d5d8e89bc77be7dc367a308
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-06-04 10:57:55 +0530

    Time: 297 ms (84.26%), Space: 18.9 MB (61.94%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -3,13 +3,18 @@ class Solution:
         if not grid:
             return 0
         def dfs(grid,row,col,r,c):
-            if r<0 or r>=row or c<0 or c>=col or grid[r][c]!="1":
-                return
+            if r<0 or r>=row or c>=col or c<0 or grid[r][c]!="1":
+                return 
             grid[r][c]="2"
             dfs(grid,row,col,r-1,c)
             dfs(grid,row,col,r+1,c)
             dfs(grid,row,col,r,c-1)
             dfs(grid,row,col,r,c+1)
+        
+            
+        
+        
+        
         count=0
         for r in range(len(grid)):
             for c in range(len(grid[0])):
@@ -17,6 +22,48 @@ class Solution:
                     count+=1
                     dfs(grid,len(grid),len(grid[0]),r,c)
         return count
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         if not grid:
+#             return 0
+#         def dfs(grid,row,col,r,c):
+#             if r<0 or r>=row or c<0 or c>=col or grid[r][c]!="1":
+#                 return
+#             grid[r][c]="2"
+#             dfs(grid,row,col,r-1,c)
+#             dfs(grid,row,col,r+1,c)
+#             dfs(grid,row,col,r,c-1)
+#             dfs(grid,row,col,r,c+1)
+#         count=0
+#         for r in range(len(grid)):
+#             for c in range(len(grid[0])):
+#                 if grid[r][c]=="1":
+#                     count+=1
+#                     dfs(grid,len(grid),len(grid[0]),r,c)
+#         return count
 
         
         
