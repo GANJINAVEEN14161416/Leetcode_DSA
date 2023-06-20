f5db98fd3802ebf41182e769e58d8a0d3b34b38a
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-06-20 12:59:42 +0530

    Added solution - LeetHub

--- Minimum number of jumps - GFG/minimum-number-of-jumps.py
+++ Minimum number of jumps - GFG/minimum-number-of-jumps.py
@@ -1,26 +1,26 @@
 #User function Template for python3
 class Solution:
-    def minJumps(self, arr, n):
-        count=end=start=0
-        for i in range(len(arr)-1):
-            start=max(start,i+arr[i])
-            if end==i:
-                end=start
-                count+=1
-            if arr[i]==0 and end==i:
-                return -1
-        return count
+	def minJumps(self, arr, n):
+	    start=end=count=0
+	    for i in range(n-1):
+	        start=max(start,i+arr[i])
+	        if end==i:
+	            end=start
+	            count+=1
+	        if arr[i]==0 and end==i:
+	            return -1
+	    return count
 
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
 if __name__ == '__main__':
-    T=int(input())
-    for i in range(T):
-        n = int(input())
-        Arr = [int(x) for x in input().split()]
-        ob = Solution()
-        ans = ob.minJumps(Arr,n)
-        print(ans)
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		Arr = [int(x) for x in input().split()]
+		ob = Solution()
+		ans = ob.minJumps(Arr,n)
+		print(ans)
 # } Driver Code Ends
