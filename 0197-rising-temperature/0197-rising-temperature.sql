bd2861617922efec9e425b33bf4041b1ded89e07
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-06-23 23:33:15 +0530

    Time: 1138 ms (16.02%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,7 +1,2 @@
-# # Write your MySQL query statement below
-# SELECT DISTINCT a.Id
-# FROM Weather a,Weather b
-# WHERE a.Temperature > b.Temperature
-# AND DATEDIFF(a.Recorddate,b.Recorddate) = 1
 select x.id from weather x,weather y
-where x.temperature>y.temperature and DATEDIFF(x.recordDate,y.recordDate)=1
+where x.temperature >y.temperature and datediff(x.recorddate,y.recorddate)=1
