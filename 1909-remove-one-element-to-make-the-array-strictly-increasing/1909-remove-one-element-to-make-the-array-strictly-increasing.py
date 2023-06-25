d4c6a35064048bde1e48dc89df675b9595a38e9e
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-06-25 12:02:21 +0530

    Time: 346 ms (27.83%), Space: 16.7 MB (6.33%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.py
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.py
@@ -1,14 +1,8 @@
 class Solution:
     def canBeIncreasing(self, nums: List[int]) -> bool:
-        isRemove = False
-        for i in range(1, len(nums)):
-            if nums[i] <= nums[i-1]:
-                if isRemove: return False
-                if i > 1 and nums[i] <= nums[i-2]:
-                    nums[i] = nums[i-1]
-                else:
-                    nums[i-1] = nums[i]
-                isRemove = True
-
-        return True 
+        for i in range(len(nums)):
+            temp=nums[:i]+nums[i+1:]
+            if temp==sorted(set(temp)):
+                return True
+        return False
         
