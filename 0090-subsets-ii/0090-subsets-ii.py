d2646c9a9f765fc87d28166648cea9f484bc819c
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-06-29 23:38:22 +0530

    Time: 59 ms (25.26%), Space: 16.4 MB (99.45%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.py
+++ 0090-subsets-ii/0090-subsets-ii.py
@@ -1,14 +1,12 @@
 class Solution:
     def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
-        list1=[[]]
-        n=len(nums)
-        for i in range(1,2**n):
-            list2=[]
-            for j in range(n):
-                if (i&1<<j):
-                    list2.append(nums[j])
-                list2.sort()
-                if list2 not in list1:
-                    list1.append(list2)
-        return list1
+        def back(nums,ans,temp):
+            ans.append(temp)
+            for i in range(len(nums)):
+                if i!=0 and nums[i]==nums[i-1]:
+                    continue
+                back(nums[i+1:],ans,temp+[nums[i]])
+        ans=[]
+        back(sorted(nums),ans,[])
+        return ans
         
