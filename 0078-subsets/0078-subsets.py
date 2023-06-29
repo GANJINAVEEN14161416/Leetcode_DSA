bdee9179b73fd231ba3340f76a14b4e44a09adb1
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-06-29 23:36:37 +0530

    Time: 51 ms (51.64%), Space: 16.5 MB (80.87%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,12 +1,10 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        list1 = [[]]
-        n=len(nums)
-        for i in range(1,2**n):
-            list2=[]
-            for j in range(n):
-                if (i&1<<j):
-                    list2.append(nums[j])
-            list1.append(list2)
-        return list1
+        def back(nums,ans,temp):
+            ans.append(temp)
+            for i in range(len(nums)):
+                back(nums[i+1:],ans,temp+[nums[i]])
+        ans=[]
+        back(nums,ans,[])
+        return ans
         
