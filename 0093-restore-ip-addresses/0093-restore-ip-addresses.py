d2ea6dc4a7f107ca8f18e79595cabce6e8d0836e
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-06-29 16:14:13 +0530

    Time: 42 ms (94.87%), Space: 16.4 MB (23.43%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,7 +1,5 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
-        ans=[]
-        k=0
         def back(s,ans,k,temp=''):
             if k==4 and len(s)==0:
                 ans.append(temp[:-1])
@@ -17,20 +15,4 @@ class Solution:
                     continue
                 back(s[i+1:],ans,k+1,temp+s[:i+1]+'.')  
             return ans
-        return back(s,ans,k,'')
-# def backtrack(self, s, ans,k, temp=''):
-#     if k==4 and len(s)==0:
-#         ans.append(temp[:-1])
-#         return
-#     if k==4 or len(s)==0:
-#         return
-#     for i in range(3):
-#         if k>4 or i+1>len(s):
-#             break
-        
-#         if int(s[:i+1])>255:
-#             continue
-#         if i != 0 and s[0]=='0':
-#             continue
-                
-#         self.backtrack(s[i+1:], ans, k+1, temp+s[:i+1]+'.')
+        return back(s,[],0,'')
