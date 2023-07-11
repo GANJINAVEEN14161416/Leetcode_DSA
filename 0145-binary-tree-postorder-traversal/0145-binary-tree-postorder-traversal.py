3071f4016a5fafacc07e05f28df0002fcc5bdea8
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-11 13:21:32 +0530

    Time: 55 ms (8.72%), Space: 16.2 MB (77.76%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.py
@@ -11,13 +11,11 @@ class Solution:
         stack.append(root)
         while stack:
             node=stack.pop()
-            ans.append(node.val)
-            
             if node.left:
                 stack.append(node.left)
             if node.right:
                 stack.append(node.right)
-            
+            ans.append(node.val)
         return ans[::-1]
             
         
