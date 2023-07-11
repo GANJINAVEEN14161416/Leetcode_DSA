42895455bd9d4fb2bb156dfbf355bcc9588277c2
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-12 01:23:15 +0530

    Time: 334 ms (66.21%), Space: 18.9 MB (64.91%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,26 +1,22 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        if not grid:
-            return 0
+        def dfs(visit,i,j,grid,m1,m2):
+            visit[i][j]=True
+            for change in range(4):
+                i1=i+m1[change]
+                j1=j+m2[change]
+                if i1>=0 and i1<len(grid) and j1>=0 and j1<len(grid[0]) and not visit[i1][j1] and grid[i1][j1]=="1":
+                    dfs(visit,i1,j1,grid,m1,m2)
+        m1=[0,1,0,-1]
+        m2=[1,0,-1,0]
         count=0
-        visit=set()
-        def bfs(grid,r,c):
-            q=deque()
-            q.append((r,c))
-            visit.add((r,c))
-            while q:
-                row,col=q.popleft()
-                direction=[[0,1],[1,0],[-1,0],[0,-1]]
-                for dr,dc in direction:
-                     r,c=dr+row,dc+col
-                     if r in range(len(grid)) and c in range(len(grid[0])) and grid[r][c]=="1" and (r,c) not in visit:
-                            visit.add((r,c))
-                            q.append((r,c))
-
-                            
-        for r in range(len(grid)):
-            for c in range(len(grid[0])):
-                if grid[r][c]=="1" and (r,c) not in visit:
+        row=len(grid)
+        col=len(grid[0])
+        visit=[[False]*col for i in range(row)]
+        for i in range(row):
+            for j in range(col):
+                if not visit[i][j] and grid[i][j]=="1":
                     count+=1
-                    bfs(grid,r,c)
+                    dfs(visit,i,j,grid,m1,m2)
+                    
         return count
