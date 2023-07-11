2b74315ae348283131e993e80a333cb2696aa896
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-12 02:18:20 +0530

    Time: 89 ms (66.46%), Space: 16.6 MB (45.96%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,23 +1,24 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-            def dfs(row,col,sr,sc,color,sourse,image):
-                if sr<0 or sr>=row or sc<0 or sc>=col:
-                    return 
-                if sourse!=image[sr][sc]:
-                    return 
-                else:
-                    image[sr][sc]=color
-                    dfs(row,col,sr-1,sc,color,sourse,image)
-                    dfs(row,col,sr+1,sc,color,sourse,image)
-                    dfs(row,col,sr,sc-1,color,sourse,image)
-                    dfs(row,col,sr,sc+1,color,sourse,image)
-            if image[sr][sc]==color:
-                return image
-            else:
-                dfs(len(image),len(image[0]),sr,sc,color,image[sr][sc],image)
-                return image
+        if image[sr][sc]==color:
+            return image
+        q=deque()
+        q.append([sr,sc])
+        temp=image[sr][sc]
+        rowmatrix=[0,-1,0,1]
+        colmatrix=[1,0,-1,0]
+        visit=[[False]*len(image[0]) for i in range(len(image))]
+        visit[sr][sc]=True
+        image[sr][sc]=color
+        while q:
+            row,col=q.popleft()
+            for i in range(4):
+                newrow=row+rowmatrix[i]
+                newcol=col+colmatrix[i]
+                if newrow>=0 and newrow<len(image) and newcol>=0 and newcol<len(image[0]) and not visit[newrow][newcol] and image[newrow][newcol]==temp:
+                    q.append([newrow,newcol])
+                    image[newrow][newcol]=color
+                    visit[newrow][newcol]=True
+        return image
             
-
-
-        
         
