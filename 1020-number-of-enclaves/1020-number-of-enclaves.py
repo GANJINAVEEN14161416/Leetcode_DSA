05b940b7fa7b52d86dfa35de8339961e1f9fc9e2
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-12 14:41:17 +0530

    Time: 708 ms (47.34%), Space: 84.9 MB (42.40%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -1,32 +1,27 @@
 class Solution:
-    def numEnclaves(self, board: List[List[int]]) -> int:
-        row=len(board)
-        col=len(board[0])
-        q=deque()
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        row=len(grid)
+        col=len(grid[0])
         visit=[[False]*col for i in range(row)]
-        for i in range(row):
-            for j in range(col):
-                if (i==0 or j==0 or j==col-1 or i==row-1) and board[i][j]==1:
-                    q.append([i,j])
-                    visit[i][j]=True
         m1=[0,-1,0,1]
         m2=[1,0,-1,0]
-        while q:
-            r,c=q.popleft()
+        count=0
+        def dfs(visit,r,c,grid):
+            visit[r][c]=True
             for new in range(4):
                 newrow=r+m1[new]
                 newcol=c+m2[new]
-                if newrow>=0 and newrow<row and newcol>=0 and newcol<col and not visit[newrow][newcol] and board[newrow][newcol]==1:
-                    visit[newrow][newcol]=True
-                    q.append([newrow,newcol])
-        count=0
+                if newrow>=0 and newcol<col and newcol>=0 and newrow<row and not visit[newrow][newcol] and grid[newrow][newcol]==1:
+                    dfs(visit,newrow,newcol,grid)
         for i in range(row):
             for j in range(col):
-                if not visit[i][j] and board[i][j]==1:
-                    count+=1
+                if (i==0 or j==0 or j==col-1 or i==row-1) and grid[i][j]==1:
                     visit[i][j]=True
+                    dfs(visit,i,j,grid)
+        for i in range(row):
+            for j in range(col):
+                if not visit[i][j] and grid[i][j]==1:
+                    count+=1
         return count
-                
-                    
         
         
