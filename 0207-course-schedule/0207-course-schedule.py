753a3e4dd70d815dda58825f99345b8cfcf3b75b
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-12 19:57:38 +0530

    Time: 126 ms (17.00%), Space: 17.8 MB (90.60%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,22 +1,25 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        dic={i:[] for i in range(numCourses)}
-        for crs,pre in prerequisites:
-            dic[crs].append(pre)
-        visit=set()
-        def dfs(crs):
-            if crs in visit:
-                return False
-            if dic[crs]==[]:
-                return True
-            visit.add(crs)
-            for pre in dic[crs]:
-                if not dfs(pre):
-                    return False
-            visit.remove(crs)
-            dic[crs]=[]
-            return True
-        for crs in range(numCourses):
-            if not dfs(crs):return False
-        return True
+        adj=defaultdict(list)
+        for i in range(len(prerequisites)):
+            adj[prerequisites[i][1]].append(prerequisites[i][0])
+        V=len(adj)
+        q=deque()
+        indegree=[0]*numCourses
+        for i in range(numCourses):
+            for j in adj[i]:
+                indegree[j]+=1
+        for i in range(numCourses):
+            if indegree[i]==0:
+                q.append(i)
+        count=0
+        while q:
+            x=q.popleft()
+            count+=1
+            for child in adj[x]:
+                indegree[child]-=1
+                if indegree[child]==0:
+                    q.append(child)
+        return count==numCourses
+                
         
