9250742e02d30977a2df126acd72cb44155eb8ea
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-14 01:38:59 +0530

    Time: 255 ms (11.33%), Space: 17.4 MB (20.05%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,22 +1,43 @@
+class Unionfind:
+    def __init__(self,n=10):
+        self.parent=[i for i in range(n+1)]
+        self.rank=[0 for i in range(n+1)]
+    def union(self,u,v):
+        parent_u=self.find(u)
+        parent_v=self.find(v)
+        if parent_u==parent_v:
+            return True
+        if self.rank[parent_u]<self.rank[parent_v]:
+            self.parent[parent_u]=parent_v
+            self.rank[parent_v]+=self.rank[parent_u]
+        else:
+            self.parent[parent_v]=parent_u
+            self.rank[parent_u]+=self.rank[parent_v]
+        return False
+
+    def find(self,u):
+        if self.parent[u]==u:
+            return u
+        self.parent[u]=self.find(self.parent[u])
+        return self.parent[u]
+
+
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        obj=Unionfind(len(isConnected))
         adj=defaultdict(list)
-        for i in range(len(isConnected)):
-            for j in range(len(isConnected)):
-                if isConnected[i][j]==1 and i!=j:
-                    adj[i+1].append(j+1)            
-        V=len(isConnected)
-        visit=[0]*(V+1)
-        def dfs(node,visit,adj):
-            visit[node]=1
-            for child in adj[node]:
-                if not visit[child]:
-                    dfs(child,visit,adj)
-            
+        n=len(isConnected)
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j]==1:
+                    obj.union(i,j)
         count=0
-        for i in range(1,V+1):
-            if not visit[i]:
+        for i in range(n):
+            if obj.find(i)==i:
                 count+=1
-                dfs(i,visit,adj)   
         return count
+        
+        
+
         
