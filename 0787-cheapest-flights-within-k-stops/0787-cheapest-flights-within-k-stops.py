355f1209fe7d5c9d856ecd2d11f3b7c39cd3d9d1
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-13 11:21:31 +0530

    Time: 113 ms (92.93%), Space: 17.5 MB (96.57%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -0,0 +1,31 @@
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        adj=defaultdict(list)
+        for innode,destnode,wt in flights:
+            adj[innode].append([destnode,wt])
+        q=deque()
+        distance=[float('inf')]*n
+#         q=deque()
+#         heapq.heappush(pq,[0,src,0])
+#         distance[src]=0
+#         while pq:
+#             steps,dis,node=heapq.heappop(pq)
+#             if dst==node:
+#                 distance[dst]
+#             for child,wt in adj[node]:
+#                 if dis+wt<distance[child] and steps<=k:
+#                     distance[child]=dis+wt
+#                     heapq.heappush(pq,[steps+1,dis+wt,child])
+#         print(distance)
+#         return distance[dst] if distance[dst]!=float('inf') else -1
+        q=deque()
+        q.append([0,src,0])
+        while q:
+            dis,node,steps=q.popleft()
+            for child,wt in adj[node]:
+                if dis+wt<distance[child] and steps<=k:
+                    distance[child]=dis+wt
+                    q.append([dis+wt,child,steps+1])
+        return distance[dst] if distance[dst]!=float('inf') else -1
+            
+        
