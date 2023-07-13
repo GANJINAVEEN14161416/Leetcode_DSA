c2d57d70ca8e53ad5dcf1667936fd94a638fbbfe
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-14 03:07:00 +0530

    Time: 558 ms (20.49%), Space: 36.3 MB (83.21%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.py
@@ -1,21 +1,42 @@
+class Unionfind:
+    def __init__(self,n):
+        self.parent=[i for i in range(n)]
+        self.rank=[0 for i in range(n)]
+    def union(self,u,v):
+        parent_u=self.find(u)
+        parent_v=self.find(v)
+        if parent_u==parent_v:
+            return True
+        if self.rank[parent_u]<self.rank[parent_v]:
+            self.parent[parent_u]=parent_v
+            self.rank[parent_v]+=self.rank[parent_u]
+        else:
+            self.parent[parent_v]=parent_u
+            self.rank[parent_u]+=self.rank[parent_v]
+
+    def find(self,u):
+        if self.parent[u]==u:
+            return u
+        self.parent[u]=self.find(self.parent[u])
+        return self.parent[u]
+
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
-        adj=defaultdict(list)
-        for i,j in connections:
-            adj[i].append(j)
-            adj[j].append(i)
-        if n-1>len(connections):
-            return -1
-        def dfs(visit,i,adj):
-            visit[i]=True
-            for j in adj[i]:
-                if not visit[j]:
-                    dfs(visit,j,adj)
         count=0
-        visit=[False]*n
-        for r in range(n):
-            if not visit[r]:
+        extrawires=0
+        obj=Unionfind(n)
+        for n1,n2 in connections:
+            if obj.find(n1)==obj.find(n2):
+                extrawires+=1
+            else:
+                obj.union(n1,n2)
+        for i in range(n):
+            if obj.find(i)==i:
                 count+=1
-                dfs(visit,r,adj)
-        return count-1
+        if extrawires>=(count-1):
+            return count-1
+        else:
+            return -1
+            
+
         
