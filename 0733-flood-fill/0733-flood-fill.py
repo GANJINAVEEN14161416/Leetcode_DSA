fc0aa7e64073e2788b01564e1109186e6b2521ea
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-15 00:47:49 +0530

    Time: 92 ms (48.83%), Space: 16.4 MB (74.43%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -1,24 +1,27 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        q=deque()
+        n=len(image)
+        m=len(image[0])
+        row=len(image)
+        col=len(image[0])
         if image[sr][sc]==color:
             return image
-        q=deque()
-        q.append([sr,sc])
         temp=image[sr][sc]
-        rowmatrix=[0,-1,0,1]
-        colmatrix=[1,0,-1,0]
-        visit=[[False]*len(image[0]) for i in range(len(image))]
-        visit[sr][sc]=True
         image[sr][sc]=color
+        q.append([sr,sc])
+        m1=[0,-1,0,1]
+        m2=[1,0,-1,0]
+        visit=[[False]*m for i in range(n)]
         while q:
-            row,col=q.popleft()
-            for i in range(4):
-                newrow=row+rowmatrix[i]
-                newcol=col+colmatrix[i]
-                if newrow>=0 and newrow<len(image) and newcol>=0 and newcol<len(image[0]) and not visit[newrow][newcol] and image[newrow][newcol]==temp:
-                    q.append([newrow,newcol])
+            r,c=q.popleft()
+            for new in range(4):
+                newrow=m1[new]+r
+                newcol=m2[new]+c
+                if newrow>=0 and newcol>=0 and newrow<row and newcol<col and not visit[newrow][newcol] and image[newrow][newcol]==temp:
                     image[newrow][newcol]=color
                     visit[newrow][newcol]=True
+                    q.append([newrow,newcol])
         return image
-            
+                
         
