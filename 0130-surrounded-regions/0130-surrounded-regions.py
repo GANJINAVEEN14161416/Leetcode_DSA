27b5553c1d094963f4801fb29cadad6788aeb37d
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-15 01:45:55 +0530

    Time: 156 ms (76.17%), Space: 17.6 MB (96.17%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,16 +3,15 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        row=len(board)
-        col=len(board[0])
-        q=deque()
-        visit=[[False]*col for i in range(row)]
-        for i in range(row):
-            for j in range(col):
-                if (i==0 or j==0 or j==col-1 or i==row-1) and board[i][j]=="O":
+        q=deque()         
+        n=len(board)
+        m=len(board[0])
+        visit=[[False]*m for i in range(n)]
+        for i in range(n):
+            for j in range(m):
+                if (i==0 or j==0 or j==m-1 or i==n-1) and (board[i][j]=="O"):
                     q.append([i,j])
                     visit[i][j]=True
-        print(visit)
         m1=[0,-1,0,1]
         m2=[1,0,-1,0]
         while q:
@@ -20,15 +19,13 @@ class Solution:
             for new in range(4):
                 newrow=r+m1[new]
                 newcol=c+m2[new]
-                if newrow>=0 and newrow<row and newcol>=0 and newcol<col and not visit[newrow][newcol] and board[newrow][newcol]=="O":
+                if newrow>=0 and newrow<n and newcol>=0 and newcol<m and board[newrow][newcol]=="O" and not visit[newrow][newcol]:
                     visit[newrow][newcol]=True
                     q.append([newrow,newcol])
-        print(visit)
-        for i in range(row):
-            for j in range(col):
+        for i in range(n):
+            for j in range(m):
                 if not visit[i][j] and board[i][j]=="O":
-                    board[i][j]='X'
-                    visit[i][j]=True
-                
-                    
+                    board[i][j]="X"
+        return board
+        
         
