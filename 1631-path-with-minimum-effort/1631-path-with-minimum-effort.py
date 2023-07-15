2eefd9e7098280cc5f206cacc388e42dac6ad40e
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-15 06:08:09 +0530

    Time: 877 ms (46.68%), Space: 17.9 MB (61.35%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,21 +1,21 @@
 class Solution:
     def minimumEffortPath(self, heights: List[List[int]]) -> int:
         q=[]
-        n=len(heights)
-        coll=len(heights[0])
-        distance=[[float('inf')]*coll for i in range(n)]
-        distance[0][0]=0
         heapq.heappush(q,[0,0,0])
+        n=len(heights)
+        m=len(heights[0])
         m1=[0,-1,0,1]
         m2=[1,0,-1,0]
+        distance=[[float('inf')]*m for i in range(n)]
+        distance[0][0]=0
         while q:
             diff,r,c=heapq.heappop(q)
-            for i in range(4):
-                row=m1[i]+r
-                col=m2[i]+c
-                if row>=0 and row<n and col>=0 and col<coll:
-                    efforts=max(abs(heights[row][col]-heights[r][c]),diff)
-                    if efforts<distance[row][col]:
-                        distance[row][col]=efforts
-                        heapq.heappush(q,[efforts,row,col])
-        return distance[-1][-1]
+            for new in range(4):
+                newrow=m1[new]+r
+                newcol=m2[new]+c
+                if newrow>=0 and newrow<n and newcol>=0 and newcol<m:
+                    efforts=max(abs(heights[newrow][newcol]-heights[r][c]),diff)
+                    if efforts<distance[newrow][newcol]:
+                        distance[newrow][newcol]=efforts
+                        heapq.heappush(q,[efforts,newrow,newcol])
+        return distance[-1][-1] if distance!=float('inf') else -1
