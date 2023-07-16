965fcb15e122681b915b26a830863aba9db1b288
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-16 14:42:15 +0530

    Added solution - LeetHub

--- Minimize the Heights II - GFG/minimize-the-heights-ii.py
+++ Minimize the Heights II - GFG/minimize-the-heights-ii.py
@@ -3,16 +3,16 @@
 class Solution:
     def getMinDiff(self, arr, n, k):
         arr.sort()
-        ans = arr[-1]- arr[0]
-        for i in range(1, n):
-            if arr[i] - k < 0:
-                 continue
-            max_ = max(arr[i-1] + k, arr[-1] - k)
-            min_ = min(arr[i] - k, arr[0] + k)
-            ans = min(ans, max_ - min_)
+        #ans=0
+        ans=(arr[-1])-(arr[0])
+        for i in range(1,n):
+            if arr[i]-k<0:
+                continue
+            mi=min(arr[i]-k,arr[0]+k)
+            mx=max(arr[i-1]+k,arr[n-1]-k)
+            ans=min(ans,mx-mi)
         return ans
             
-                
 
 
 #{ 
