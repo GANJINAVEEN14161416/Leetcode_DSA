13581667c20be76919f718a32394f3a1ee525783
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-16 12:16:30 +0530

    Time: 46 ms (73.22%), Space: 16.4 MB (32.10%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.py
@@ -6,18 +6,21 @@
 #         self.right = right
 class Solution:
     def isCousins(self, root: Optional[TreeNode], x: int, y: int) -> bool:
-        nodes = collections.defaultdict(list)
-        queue = [(root,0,0)]
-        while queue:
-            node,level,parent = queue.pop(0)
-            nodes[node.val] = [level,parent]
-
+        list1=defaultdict(list)
+        q=deque()
+        q.append([root,0,0])
+        while q:
+            node,level,parent=q.popleft()
+            list1[node.val]=[level,parent]
             if node.left:
-                queue.append((node.left,level+1,node.val))
+                q.append([node.left,level+1,node.val])
             if node.right:
-                queue.append((node.right,level+1,node.val))
-
-        if nodes[x][0]==nodes[y][0] and nodes[x][1] != nodes[y][1]:
+                q.append([node.right,level+1,node.val])
+        if list1[x][0]==list1[y][0] and list1[x][1]!=list1[y][1]:
             return True
-
         return False
+    
+    
+    
+            
+        
