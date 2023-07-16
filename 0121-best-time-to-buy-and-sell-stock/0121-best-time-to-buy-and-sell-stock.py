12d8762480cb3ad12007a914b614d7b793035cf0
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-17 00:32:17 +0530

    Time: 1017 ms (58.13%), Space: 27.4 MB (23.66%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,13 +1,9 @@
 class Solution:
     def maxProfit(self, arr: List[int]) -> int:
-        buy=arr[0]
-        n=len(arr)
-        profit=0
+        buy,n,profit=arr[0],len(arr),0
         for i in range(1,n):
-            if arr[i]<buy:
-                buy=arr[i]
-            if arr[i]>buy:
-                profit=max(profit,arr[i]-buy)
+            if arr[i]<buy: buy=arr[i]
+            if arr[i]>buy:profit=max(profit,arr[i]-buy) 
         return profit
                 
         
