1d75c7f8cb33e3e989af01789c2df92a5bf965f7
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-19 00:46:00 +0530

    Added solution - LeetHub

--- Coin Change - GFG/coin-change.py
+++ Coin Change - GFG/coin-change.py
@@ -2,20 +2,16 @@
 
 class Solution:
     def count(self, wt, n, W):
-        t=[[-1]*(W+1) for i in range(n+1)]
+        t=[[0]*(W+1) for i in range(n+1)]
         for i in range(n+1):
-            for j in range(W+1):
-                if j==0:
-                    t[i][j]=1
-                elif i==0:
-                    t[i][j]=0
+            t[i][0]=1
         for i in range(1,n+1):
             for j in range(1,W+1):
                 if wt[i-1]<=j:
                     t[i][j]=t[i][j-wt[i-1]]+t[i-1][j]
                 else:
                     t[i][j]=t[i-1][j]
-        return t[n][W]
+        return t[-1][-1]
 
 
 
