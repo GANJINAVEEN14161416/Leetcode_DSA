e6b389f7364a5c5cbcad9b9446be12472544075c
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-18 22:29:18 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.py
+++ Subset Sum Problem - GFG/subset-sum-problem.py
@@ -1,22 +1,26 @@
 #User function Template for python3
 
 class Solution:
-    def isSubsetSum (self, n, wt, W):
-        t=[[-1]*(W+1) for i in range(n+1)]
+    def isSubsetSum (self, n, arr, W):
+        t=[[False]*(W+1) for i in range(N+1)]
         for i in range(n+1):
-            for j in range(W+1):
-                if i==0:
-                    t[i][j]=False
-                if j==0:
-                    t[i][j]=True
+            t[i][0]=True
         for i in range(1,n+1):
             for j in range(1,W+1):
-                if wt[i-1]<=j:
-                    t[i][j]=t[i-1][j-wt[i-1]] or t[i-1][j]
+                if arr[i-1]<=j:
+                    t[i][j]=t[i-1][j-arr[i-1]] or t[i-1][j]
                 else:
                     t[i][j]=t[i-1][j]
-        #print(t)
         return t[-1][-1]
+                
+        
+        
+        
+        
+        
+        
+        
+        
         
         
 
