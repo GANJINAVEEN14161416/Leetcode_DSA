3151549aaf4e4f27d935f671b7d20628db2ee8e5
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-18 23:21:17 +0530

    Added solution - LeetHub

--- Minimum sum partition - GFG/minimum-sum-partition.py
+++ Minimum sum partition - GFG/minimum-sum-partition.py
@@ -1,25 +1,26 @@
 #User function Template for python3
 class Solution:
 	def minDifference(self, arr, n):
-		total_sum = sum(arr)
-
-        dp = [[False]*(total_sum+1) for i in range(n+1)]
-        for i in range(n+1):
-            dp[i][0] = True
-
-        for i in range(1, n+1):
-            for j in range(1, total_sum+1):
-                if arr[i-1] > j:
-                    dp[i][j] = dp[i-1][j]
-                else:
-                    dp[i][j] = dp[i-1][j] or dp[i-1][j-arr[i-1]]
-
-        min_diff = float('inf')
-        for j in range(total_sum//2+1):
-            if dp[n][j]:
-                min_diff = min(min_diff, total_sum -2*j)
-        return min_diff
-
+	    x=sum(arr)
+        def subset(N,arr,W):
+            x=sum(arr)
+            dp=[[False]*(W+1) for i in range(N+1)]
+            for i in range(N+1):
+                dp[i][0]=True
+            for i in range(1,N+1):
+                for j in range(1,W+1):
+                    if arr[i-1]>j:
+                        dp[i][j]=dp[i-1][j]
+                    else:
+                        dp[i][j]=(dp[i-1][j-arr[i-1]] or dp[i-1][j])
+            ans=float('inf')
+            for i in range(W+1):
+                if dp[n][i]:
+                    ans=min(ans,x-2*i)
+            return ans
+        return subset(N,arr,x//2)
+        
+        
 
 #{ 
  # Driver Code Starts
