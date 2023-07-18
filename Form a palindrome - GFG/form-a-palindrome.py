353f5913d98570516eb8d38b795ccf4b89122275
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-19 02:51:44 +0530

    Added solution - LeetHub

--- Form a palindrome - GFG/form-a-palindrome.py
+++ Form a palindrome - GFG/form-a-palindrome.py
@@ -1,19 +1,20 @@
 #User function Template for python3
 
 class Solution:
-    def countMin(self, Str):
-        s1 = Str
-        n = len(s1)
-        s2 = s1[::-1]
-        dp = [[0 for i in range(n+1)] for j in range(n+1)]
-
-        for i in range(n-1,-1,-1):
-            for j in range(n-1,-1,-1):
-                if s1[i] == s2[j]:
-                    dp[i][j] = 1 + dp[i+1][j+1]
+    def countMin(self, S):
+        # code here
+        s1=S
+        s2=S[::-1]
+        x,y=len(s1),len(s2)
+        t=[[0]*(y+1) for _ in range(x+1)]
+        for i in range(1,x+1):
+            for j in range(1,y+1):
+                if s1[i-1]==s2[j-1]:
+                    t[i][j]=1+t[i-1][j-1]
                 else:
-                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
-        return n - dp[0][0]
+                    t[i][j]=max(t[i-1][j],t[i][j-1])
+        a=t[-1][-1]
+        return len(S)-a
 
 
 #{ 
