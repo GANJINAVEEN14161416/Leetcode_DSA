3a6ed6490314d213e8b63cb8e97fc637288ade51
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-20 16:04:26 +0530

    Time: 1486 ms (39.99%), Space: 17.6 MB (59.78%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.py
@@ -1,17 +1,19 @@
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
-        memo = {}
-        cuts = [0] + sorted(cuts) + [n]
-        
-        def cost(left, right):
-            if (left, right) in memo:
-                return memo[(left, right)]
-            if right - left == 1:
+        le=len(cuts)
+        cuts=[0]+sorted(cuts)+[n]
+        dp=[[-1]*(le+1) for i in range(le+1)]
+        def solve(i,j,cuts):
+            if i>j:
                 return 0
-            ans = min(cost(left, mid) + cost(mid, right) + cuts[right] - cuts[left] for mid in range(left + 1, right))
-            memo[(left, right)] = ans
-            return ans
-        
-        return cost(0, len(cuts) - 1)
+            if dp[i][j]!=-1:
+                return dp[i][j]
+            mini=float('inf')
+            for k in range(i,j+1):
+                steps=cuts[j+1]-cuts[i-1]+solve(i,k-1,cuts)+solve(k+1,j,cuts)
+                mini=min(mini,steps)
+            dp[i][j]=mini
+            return mini
+        return solve(1,le,cuts)
         
         
