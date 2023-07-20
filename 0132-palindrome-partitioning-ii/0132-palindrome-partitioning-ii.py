220fc2131ecd90cfbea7e897934154878217ffc4
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-21 00:41:03 +0530

    Time: 4176 ms (17.02%), Space: 16.5 MB (87.05%) - LeetHub

---
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minCut(self, string: str) -> int:
+        def ispalindrome(s):
+            return s==s[::-1]
+        n=len(string)
+        dp=[0]*(n+1)
+        dp[n]=0
+        for i in range(n-1,-1,-1):
+            temp=""
+            min_cost=float('inf')
+            for j in range(i,n):
+                temp+=string[j]
+                if (ispalindrome(temp)):
+                    cost=1+dp[j+1]
+                    min_cost=min(min_cost,cost)
+            dp[i]= min_cost
+        return dp[0]-1
+        
