42a41763d5b3df5aa7e319c31a1f6c9b68b87835
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-20 15:13:05 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.py
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.py
@@ -3,38 +3,18 @@
 class Solution:
     def matrixMultiplication(self, N, arr):
         t=[[-1]*N for i in range(N)]
-        def solve(arr,i,j):
-            if i>=j:
-                return 0
-            if t[i][j]!=-1:
-                return t[i][j]
-            mini=float('inf')
-            for k in range(i,j):
-                steps=arr[i-1]*arr[k]*arr[j]+solve(arr,i,k)+solve(arr,k+1,j)
-                if steps<mini:
-                    mini=steps
-            t[i][j]=mini
-            return mini
-        return solve(arr,1,N-1)
-        
-            
-        
-        
-    #     dp=[[-1]*N for _ in range(N)]
-    #     return self.solve(1,N-1,arr,dp)
-    
-    # def solve(self,i,j,arr,dp):
-    #     if i>=j:
-    #         return 0
-    #     if dp[i][j]!=-1:
-    #         return dp[i][j]
-            
-    #     temp=float('inf')
-    #     for k in range(i,j):
-    #         ans=self.solve(i,k,arr,dp)+self.solve(k+1,j,arr,dp)+arr[i-1]*arr[k]*arr[j]
-    #         temp=min(temp,ans)
-    #     dp[i][j]=temp
-    #     return temp
+        for i in range(1,N):
+            t[i][i]=0
+        for i in range(N-1,0,-1):
+            for j in range(i+1,N):
+                mini=float('inf')
+                for k in range(i,j):
+                    steps=arr[i-1]*arr[k]*arr[j]+t[i][k]+t[k+1][j]
+                    if steps<mini:
+                        mini=steps
+                t[i][j]=mini
+        return t[1][N-1]
+                
 
 
 #{ 
