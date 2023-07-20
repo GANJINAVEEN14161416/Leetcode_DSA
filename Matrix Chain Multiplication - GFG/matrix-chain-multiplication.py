31ce38f8e954e54265e61c62d6c3486daa6a0d03
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-20 14:29:11 +0530

    Added solution - LeetHub

--- Matrix Chain Multiplication - GFG/matrix-chain-multiplication.py
+++ Matrix Chain Multiplication - GFG/matrix-chain-multiplication.py
@@ -2,21 +2,39 @@
 
 class Solution:
     def matrixMultiplication(self, N, arr):
-        dp=[[-1]*N for _ in range(N)]
-        return self.solve(1,N-1,arr,dp)
-    
-    def solve(self,i,j,arr,dp):
-        if i>=j:
-            return 0
-        if dp[i][j]!=-1:
-            return dp[i][j]
+        t=[[-1]*N for i in range(N)]
+        def solve(arr,i,j):
+            if i>=j:
+                return 0
+            if t[i][j]!=-1:
+                return t[i][j]
+            mini=float('inf')
+            for k in range(i,j):
+                steps=arr[i-1]*arr[k]*arr[j]+solve(arr,i,k)+solve(arr,k+1,j)
+                if steps<mini:
+                    mini=steps
+            t[i][j]=mini
+            return mini
+        return solve(arr,1,N-1)
+        
             
-        temp=float('inf')
-        for k in range(i,j):
-            ans=self.solve(i,k,arr,dp)+self.solve(k+1,j,arr,dp)+arr[i-1]*arr[k]*arr[j]
-            temp=min(temp,ans)
-        dp[i][j]=temp
-        return temp
+        
+        
+    #     dp=[[-1]*N for _ in range(N)]
+    #     return self.solve(1,N-1,arr,dp)
+    
+    # def solve(self,i,j,arr,dp):
+    #     if i>=j:
+    #         return 0
+    #     if dp[i][j]!=-1:
+    #         return dp[i][j]
+            
+    #     temp=float('inf')
+    #     for k in range(i,j):
+    #         ans=self.solve(i,k,arr,dp)+self.solve(k+1,j,arr,dp)+arr[i-1]*arr[k]*arr[j]
+    #         temp=min(temp,ans)
+    #     dp[i][j]=temp
+    #     return temp
 
 
 #{ 
