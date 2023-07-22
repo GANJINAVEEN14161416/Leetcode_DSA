1f7cea6e8f75b00447f9ddd8c393fc23f1241831
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-23 02:57:07 +0530

    Added solution - LeetHub

--- Kth smallest element - GFG/kth-smallest-element.py
+++ Kth smallest element - GFG/kth-smallest-element.py
@@ -2,8 +2,37 @@
 import heapq
 class Solution:
     def kthSmallest(self,arr, l, r, k):
-        list1=heapq.nsmallest(k,arr)
-        return  list1[k-1]
+        def heapify(arr, N, i):
+         
+            largest = i  # Initialize largest as root
+            l = 2 * i + 1  # left = 2*i + 1
+            r = 2 * i + 2  # right = 2*i + 2
+         
+            # If left child is larger than root
+            if l < N and arr[l] < arr[largest]:
+                largest = l
+         
+            # If right child is larger than largest so far
+            if r < N and arr[r] < arr[largest]:
+                largest = r
+         
+            # If largest is not root
+            if largest != i:
+                arr[i], arr[largest] = arr[largest], arr[i]
+         
+                # Recursively heapify the affected sub-tree
+                heapify(arr, N, largest)
+        def buildHeap(arr, N):
+            startIdx = N // 2 - 1
+            for i in range(startIdx, -1, -1):
+                heapify(arr, N, i)
+        buildHeap(arr,len(arr))
+        for i in range(len(arr)):
+           x= heapq.heappop(arr)
+           if i==(k-1):
+               return x
+        return x
+            
 
 
 #{ 
