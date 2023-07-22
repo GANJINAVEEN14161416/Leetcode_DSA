6ada47dbf0bbbd728a5bfe52d62ecf97fa8c7a35
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-22 18:19:03 +0530

    Time: 51 ms (36.83%), Space: 16.3 MB (80.93%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -3,9 +3,9 @@ class Solution:
         prev =[0]*(n+1)
         cur=[0]*(n+1)
         for ind1 in range(1,m+1):
-            for ind2 in range(n+1):
-                if ind1==0 or ind2==0:
-                    prev[0]=0
+            for ind2 in range(1,n+1):
+                # if ind1==0 or ind2==0:
+                #     prev[0]=0
                 if ind1==1 and ind2==1:
                     cur[1]=1
                 else:
