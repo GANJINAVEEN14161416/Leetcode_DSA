df605975a9c8f8d0f1466699b623d44263e7380c
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-22 13:58:11 +0530

    Added solution - LeetHub

--- Introduction to DP - GFG/introduction-to-dp.py
+++ Introduction to DP - GFG/introduction-to-dp.py
@@ -4,15 +4,20 @@
 
 # } Driver Code Ends
 #User function Template for python3
-
+from sys import setrecursionlimit
+setrecursionlimit(10**9)
 class Solution:
     def topDown(self, n):
-        dp=[0]*(n+1)
-        dp[0]=0
-        dp[1]=1
-        for i in range(2,n+1):
-            dp[i]=dp[i-1]+dp[i-2]
-        return dp[n]%(10**9+7)
+        dp=[-1]*(n+1)
+        def solve(n):
+            if n==0 or n==1:
+                dp[n]=n
+                return dp[n]
+            if dp[n]!=-1:
+                return dp[n]
+            dp[n]=solve(n-1)+solve(n-2)
+            return dp[n]
+        return solve(n)%(10**9+7)
     def bottomUp(self, n):
         # Code here
         dp=[0]*(n+1)
@@ -21,6 +26,9 @@ class Solution:
         for i in range(2,n+1):
             dp[i]=dp[i-1]+dp[i-2]
         return dp[n]%(10**9+7)
+        
+        
+        
 
 #{ 
  # Driver Code Starts.
