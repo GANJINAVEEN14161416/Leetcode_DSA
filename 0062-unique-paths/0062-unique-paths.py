b61b29e098f10aa10497ef83cbe48bc8e16b8a24
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-22 18:18:18 +0530

    Time: 47 ms (63.05%), Space: 16.3 MB (51.67%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,8 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
         prev =[0]*(n+1)
+        cur=[0]*(n+1)
         for ind1 in range(1,m+1):
-            cur=[0]*(n+1)
             for ind2 in range(n+1):
                 if ind1==0 or ind2==0:
                     prev[0]=0
