0b6db6a449d60fe9c64398d03717704850ae44ff
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-22 14:16:50 +0530

    Added solution - LeetHub

--- Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.py
+++ Count ways to reach the n'th stair - GFG/count-ways-to-reach-the-nth-stair.py
@@ -4,11 +4,12 @@ class Solution:
     def countWays(self,n):
         pre1=1
         pre2=1
+        temp=1
         for i in range(2,n+1):
             temp=pre1+pre2
             pre2=pre1
             pre1=temp
-        return pre1%(10**9+7)
+        return temp%(10**9+7)
 
 
 
