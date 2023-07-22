80f4ac99aff069ab4f45b02cd88b6913cf0c755d
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-22 17:43:11 +0530

    Time: 50 ms (42.23%), Space: 16.5 MB (12.30%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,15 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp=[[-1]*n for i in range(m)]
+        dp=[[-1]*(n+1) for i in range(m+1)]
         def solve(ind1,ind2):
-            if ind1==0 and ind2==0:
+            if ind1==1 and ind2==1:
                 return 1
-            if ind1<0  or ind2<0:
+            if ind1==0  or ind2==0:
                 return 0
             if dp[ind1][ind2]!=-1:
                 return dp[ind1][ind2]
             dp[ind1][ind2]=solve(ind1-1,ind2)+solve(ind1,ind2-1)
             return dp[ind1][ind2]
-        return solve(m-1,n-1)
-        return dp[-1][-1]
-        
+        return solve(m,n)
