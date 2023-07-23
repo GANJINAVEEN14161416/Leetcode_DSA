afb6cc58e46c320daac930f6e98c01950832f53e
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-23 05:34:29 +0530

    Added solution - LeetHub

---
+++ Chocolate Distribution Problem - GFG/chocolate-distribution-problem.py
@@ -0,0 +1,35 @@
+#User function Template for python3
+
+class Solution:
+
+    def findMinDiff(self, arr,N,M):
+        arr.sort()
+        ans=float('inf')
+        left,right=0,M-1
+        while right<N:
+            if ans<0:
+            ans=min(ans,arr[right]-arr[left])
+            left+=1
+            right+=1
+        return ans
+        # code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        N = int(input())
+        A = [int(x) for x in input().split()]
+        M = int(input())
+
+
+        solObj = Solution()
+
+        print(solObj.findMinDiff(A,N,M))
+# } Driver Code Ends
