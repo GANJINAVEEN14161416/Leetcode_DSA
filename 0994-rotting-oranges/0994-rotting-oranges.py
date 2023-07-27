ebb3b805457928250f472c5499fae12c0d891360
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-07-27 14:47:37 +0530

    Time: 68 ms (56.38%), Space: 16.4 MB (27.67%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -16,16 +16,15 @@ class Solution:
         time=0
         count=0
         while q:
-            for i in range(len(q)):
-                r,c,steps=q.popleft()
-                time=max(time,steps)
-                for new in range(4):
-                    newrow=r+m1[new]
-                    newcol=c+m2[new]
-                    if newrow>=0 and newrow<n and newcol<m and newcol>=0 and not visit[newrow][newcol] and grid[newrow][newcol]==1:
-                        visit[newrow][newcol]=True
-                        q.append([newrow,newcol,steps+1])
-                        count+=1
+            r,c,steps=q.popleft()
+            time=max(time,steps)
+            for new in range(4):
+                newrow=r+m1[new]
+                newcol=c+m2[new]
+                if newrow>=0 and newrow<n and newcol<m and newcol>=0 and not visit[newrow][newcol] and grid[newrow][newcol]==1:
+                    visit[newrow][newcol]=True
+                    q.append([newrow,newcol,steps+1])
+                    count+=1
         return time if count==fresh else -1
                         
             
