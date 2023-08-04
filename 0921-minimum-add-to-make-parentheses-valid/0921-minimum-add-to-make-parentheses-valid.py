30ee07a5adbfa7f44047c46fb8eb15db74a93126
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-04 20:13:47 +0530

    Time: 42 ms (76.92%), Space: 16.4 MB (34.85%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minAddToMakeValid(self, s: str) -> int:
+        ans = bal = 0
+        for symbol in s:
+            bal += 1 if symbol == '(' else -1
+            # It is guaranteed bal >= -1
+            if bal == -1:
+                ans += 1
+                bal += 1
+        return ans + bal
