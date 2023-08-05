4a40c04e9f59c898a134adbc0d3e8c81475c7a3c
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-05 07:38:43 +0530

    Added solution - LeetHub

---
+++ Index of the First Occurrence of pattern in a text - GFG/index-of-the-first-occurrence-of-pattern-in-a-text.py
@@ -0,0 +1,30 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    def findMatching(self, text, pattern):
+        # Code here
+        m=len(text)
+        n=len(pattern)
+        for i in range(m-n+1):
+            if text[i:i+n]==pattern:
+                return i
+        return -1
+        
+        
+        
+
+#{ 
+ # Driver Code Starts.
+if __name__ == '__main__': 
+    t = int(input ())
+    for _ in range (t):
+        text, pattern = input().split()
+        ob = Solution()
+        res = ob.findMatching(text, pattern)
+        print(res)
+# } Driver Code Ends
