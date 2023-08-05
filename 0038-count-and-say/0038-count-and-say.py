697e526b5f8f0ed367c53fbc77fc88cf35196b2e
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-05 07:35:27 +0530

    Time: 47 ms (93.45%), Space: 16.4 MB (41.30%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.py
+++ 0038-count-and-say/0038-count-and-say.py
@@ -2,17 +2,17 @@ class Solution:
     def countAndSay(self, n: int) -> str:
         first="1"
         for i in range(n-1):
-            new=""
-            count=1
+            s=""
             current=first[0]
+            count=1
             for c in first[1:]:
-                if current==c:
+                if c==current:
                     count+=1
                 else:
-                    new+=str(count)+current
+                    s+=str(count)+current
                     current=c
                     count=1
-            new+=str(count)+current
-            first=new
+            s+=str(count)+current
+            first=s
         return first
         
