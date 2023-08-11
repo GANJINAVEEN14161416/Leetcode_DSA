cc0380a5238f2b1c6d189c375c71233f2c9b572d
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-11 14:38:33 +0530

    Time: 87 ms (96.09%), Space: 19.7 MB (99.09%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -4,16 +4,34 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        list1=[]
-        linked1=linked2=ListNode(0)
-        for i in lists:
-            while i:
-                list1.append(i)
-                i=i.next
-        list1.sort(key=lambda x:x.val)
-        for i in list1:
-            linked1.next=i
-            linked1=linked1.next
-        return linked2.next
+    def mergeKLists(self, arr: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not arr:
+            return 
+        def merge(l1,l2):
+            dummy=ListNode(-1)
+            cur=dummy
+            while l1 and l2:
+                if l1.val<=l2.val:
+                    cur.next=l1
+                    cur=cur.next
+                    l1=l1.next
+                else:
+                    cur.next=l2
+                    cur=cur.next
+                    l2=l2.next
+            if l1:
+                cur.next=l1
+            if l2:
+                cur.next=l2
+            return dummy.next
+        def mergesort(arr):
+            while len(arr)>1:
+                merged=[]
+                for i in range(0,len(arr),2):
+                    l1=arr[i]
+                    l2=arr[i+1] if (i+1)<len(arr) else None
+                    merged.append(merge(l1,l2))
+                arr=merged
+            return arr[0]
+        return mergesort(arr)
 
