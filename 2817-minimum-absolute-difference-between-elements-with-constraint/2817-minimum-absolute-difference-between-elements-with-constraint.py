fe728f6e6893cba1641dfd0ec3741c8af53d8442
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-13 09:42:11 +0530

    Time: 1243 ms (100.00%), Space: 50.5 MB (8.33%) - LeetHub

--- 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.py
+++ 2817-minimum-absolute-difference-between-elements-with-constraint/2817-minimum-absolute-difference-between-elements-with-constraint.py
@@ -1,20 +1,13 @@
 class Solution:
     def minAbsoluteDifference(self, nums: List[int], k: int) -> int:
         if not k:  return 0
-        
-        nums=sorted((val,idx) for idx,val in enumerate(nums)) 
-
+        nums=sorted((val,idx) for idx,val in enumerate(nums))
         stack1, stack2, res =[], [], float('inf')  
-
         for val,idx in nums:
-
             while stack1 and stack1[0][0]<=idx:                
                  res=min(res, val-heapq.heappop(stack1)[1])  
-
             while stack2 and -stack2[0][0]>=idx:                
                 res=min(res, val-heapq.heappop(stack2)[1]) 
-
             heapq.heappush(stack1,(idx+k,val)) 
             heapq.heappush(stack2,(-idx+k,val))   
-
         return res
