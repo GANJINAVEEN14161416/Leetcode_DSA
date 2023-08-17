bdc86016d1d40587856d33c773134a8f3b426b79
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-17 07:53:25 +0530

    Time: 62 ms (94.93%), Space: 17.3 MB (87.71%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,13 +1,15 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         m=len(triangle)
-        dp=[[0]*(m) for i in range(m)]
+        nxt=[0]*m
         for ind1 in range(m-1,-1,-1):
+            cur=[0]*m
             for ind2 in range(ind1,-1,-1):
                 if ind1==(m-1):
-                    dp[ind1][ind2]=triangle[ind1][ind2]
+                    cur[ind2]=triangle[ind1][ind2]
                 else:
-                    downleft_diag=triangle[ind1][ind2]+dp[ind1+1][ind2]
-                    downright_diag=triangle[ind1][ind2]+dp[ind1+1][ind2+1]
-                    dp[ind1][ind2]=min(downleft_diag,downright_diag)
-        return dp[0][0]
+                    downleft_diag=triangle[ind1][ind2]+nxt[ind2]
+                    downright_diag=triangle[ind1][ind2]+nxt[ind2+1]
+                    cur[ind2]=min(downleft_diag,downright_diag)
+            nxt=cur
+        return nxt[0]
