804e0a541f29ea09cc1932867335bcc55ad1e127
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-21 16:32:32 +0530

    Added solution - LeetHub

--- Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.py
+++ Searching an element in a sorted array - GFG/searching-an-element-in-a-sorted-array.py
@@ -3,9 +3,15 @@
 class Solution:
     ##Complete this function
     def searchInSorted(self,arr, N, k):
-        #Your code here
-        if k in arr:
-            return 1
+        left,right=0,N-1
+        while left<=right:
+            mid=(left+right)//2
+            if arr[mid]==k:
+                return 1
+            elif arr[mid]<k:
+                left=mid+1
+            else:
+                right=mid-1
         return -1
 
 
