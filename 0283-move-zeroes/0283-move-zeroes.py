6c0f32d94f8041fe4f811a7b606deb702d58e58b
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-21 16:23:21 +0530

    Time: 142 ms (89.95%), Space: 17.9 MB (48.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,9 +1,12 @@
 class Solution:
     def moveZeroes(self, nums: List[int]) -> None:
-        anchor = 0
-        for explorer in range(len(nums)):
-            if nums[explorer] != 0:
-                nums[anchor], nums[explorer] = nums[explorer], nums[anchor]
-                anchor += 1
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        left=0
+        for right in range(len(nums)):
+            if nums[right]!=0:
+                nums[right],nums[left]=nums[left],nums[right]
+                left+=1
         
         
