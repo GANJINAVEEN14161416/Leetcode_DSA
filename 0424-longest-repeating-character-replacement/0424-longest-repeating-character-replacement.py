41df0e00763377b2eeb2f67dfcd795210e7136ce
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-21 11:42:18 +0530

    Time: 76 ms (97.54%), Space: 16.4 MB (42.36%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -5,7 +5,7 @@ class Solution:
         for right in range(len(s)):
             dic[s[right]]+=1
             maxf=max(maxf,dic[s[right]])
-            if (right-left+1)-maxf>k:
+            if (right-left+1)>maxf+k:
                 dic[s[left]]-=1
                 left+=1
         return len(s)-left
