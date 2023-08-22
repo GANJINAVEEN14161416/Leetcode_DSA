18b8b197ea19cd0ee3d929fe34f96cf4b4e8497e
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-22 11:38:14 +0530

    Time: 111 ms (93.64%), Space: 17.3 MB (55.97%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -5,17 +5,23 @@ class Solution:
         """
         m=len(matrix)
         n=len(matrix[0])
-        arr=[]
         for i in range(m):
             for j in range(n):
                 if matrix[i][j]==0:
-                    arr.append([i,j])
-                
-        for k,l in arr:
-            for row in range(n):
-                matrix[k][row]=0
-            for col in range(m):
-                matrix[col][l]=0
+                    for row in range(n):
+                        if matrix[i][row]!=0:
+                            matrix[i][row]=-2**32
+                    for col in range(m):
+                        if matrix[col][j]!=0:
+                            matrix[col][j]=-2**32
+
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j]==(-2**32):
+                    matrix[i][j]=0
+            
+
+
         
         
         
