d03e24c5f1986f9ec69a6ab38f2d1fc39092dbfd
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-22 16:18:51 +0530

    Added solution - LeetHub

---
+++ Largest subarray with 0 sum - GFG/largest-subarray-with-0-sum.py
@@ -0,0 +1,25 @@
+#Your task is to complete this function
+#Your should return the required output
+class Solution:
+    def maxLen(self, n, arr):
+        dic={0:-1}
+        ans,prefix=0,0
+        for i in range(n):
+            prefix+=arr[i]
+            if prefix in dic:
+                ans=max(ans,i-dic[prefix])
+            if prefix not in dic:
+                dic[prefix]=i
+        return ans
+
+#{ 
+ # Driver Code Starts
+if __name__=='__main__':
+    t= int(input())
+    for i in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        ob = Solution()
+        print(ob.maxLen(n ,arr))
+# Contributed by: Harshit Sidhwa
+# } Driver Code Ends
