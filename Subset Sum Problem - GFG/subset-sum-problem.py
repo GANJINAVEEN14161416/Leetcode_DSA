40a8d0e49e7228acd7812f387fdfaa832700f511
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-23 17:16:22 +0530

    Added solution - LeetHub

--- Subset Sum Problem - GFG/subset-sum-problem.py
+++ Subset Sum Problem - GFG/subset-sum-problem.py
@@ -4,25 +4,18 @@ class Solution:
     def isSubsetSum (self, N, arr, sum):
         # code here
         dp=[[-1]*(sum+1) for i in range(N+1)]
-        def solve(ind,target):
-            if ind==0:
-                if arr[ind]==target:
-                    dp[ind][target]=True
-                else:
-                    dp[ind][target]=False
-                return dp[ind][target]
-            if target==0:
-                dp[ind][target]=True
-                return dp[ind][target]
-            if dp[ind][target]!=-1:
-                return dp[ind][target]
-            nottake=solve(ind-1,target)
-            take=False
-            if target>=arr[ind]:
-                take=solve(ind-1,target-arr[ind])
-            dp[ind][target]=take or nottake
-            return take or nottake
-        return solve(N-1,sum)
+        for i in range(sum+1):
+            dp[0][i]=False
+        for i in range(N+1):
+            dp[i][0]=True
+        for ind in range(1,N+1):
+            for target in range(1,sum+1):
+                nottake=dp[ind-1][target]
+                take=False
+                if target>=arr[ind-1]:
+                    take=dp[ind-1][target-arr[ind-1]]
+                dp[ind][target]=take or nottake
+        return dp[N][sum]
             
         
         
