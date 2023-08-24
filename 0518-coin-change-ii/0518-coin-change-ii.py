95b6092539385028ef223964d7b9b0e9f8482e84
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-24 22:21:51 +0530

    Time: 102 ms (98.28%), Space: 37.4 MB (33.16%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,20 +1,27 @@
 class Solution:
-    def change(self, W: int, wt: List[int]) -> int:
-        n=len(wt)
-        t=[[-1]*(W+1) for i in range(n+1)]
-        for i in range(n+1):
-            for j in range(W+1):
-                if i==0 and j==0:
-                    t[i][j]=-1
-                elif j==0:
-                    t[i][j]=1
-                elif i==0:
-                    t[i][j]=0
-        for i in range(1,n+1):
-            for j in range(1,W+1):
-                if wt[i-1]<=j:
-                    t[i][j]=t[i][j-wt[i-1]]+t[i-1][j]
+    def change(self, amount: int, coins: List[int]) -> int:
+        n=len(coins)
+        dp=[[-1]*(amount+1) for i in range(n+1)]
+        def solve(ind,target):
+            if target==0:
+                dp[ind][target]=1
+                return 1
+            if ind==0:
+                if target%coins[0]==0:
+                    dp[ind][target]=1
+                    return 1
                 else:
-                    t[i][j]=t[i-1][j]
-        return t[n][W]
+                    dp[ind][target]=0
+                    return 0
+            if dp[ind][target]!=-1:
+                return dp[ind][target]
+            notpick=solve(ind-1,target)
+            pick=0
+            if target>=coins[ind]:
+                pick=solve(ind,target-coins[ind])
+            dp[ind][target]=pick+notpick
+            return pick+notpick
+        return solve(n-1,amount)
+        
+        
         
