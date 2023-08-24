c509660eb100ce8caa98ec502236391920b5db1b
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-24 15:14:08 +0530

    Time: 1904 ms (8.18%), Space: 20.7 MB (25.28%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,25 +1,21 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n=len(coins)
-        dp=[[-1]*(amount+1) for i in range(n+1)]
-        def solve(ind,target):
-            if ind==0:
-                if target%coins[0]==0:
-                    dp[ind][target]=target//coins[0]
-                    return target//coins[0]
-                else:
-                    return float('inf') 
-            if dp[ind][target]!=-1:
-                return dp[ind][target]
-            notpick=solve(ind-1,target)
-            pick=float('inf')
-            if coins[ind]<=target:
-                pick=1+solve(ind,target-coins[ind])
-            dp[ind][target]=min(pick,notpick)
-            return min(pick,notpick)
-        ans=solve(n-1,amount)
-        if ans==float('inf'):
+        dp=[[0]*(amount+1) for i in range(n+1)]
+        for i in range(amount+1):
+            if i%coins[0]==0:
+                dp[0][i]=i//coins[0]
+            else:
+                dp[0][i]=float('inf')
+        for ind in range(1,n+1):
+            for target in range(1,amount+1):
+                notpick=dp[ind-1][target]
+                take=float('inf')
+                if coins[ind-1]<=target:
+                    take=1+dp[ind][target-coins[ind-1]]
+                dp[ind][target]=min(take,notpick)
+        if dp[n][amount]==float('inf'):
             return -1
-        return ans
+        return dp[n][amount]
         
         
