ad636b76f46b8de7c59e86017238231e3fc0fe1d
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-24 23:44:19 +0530

    Time: 548 ms (93.02%), Space: 41.6 MB (73.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,19 +1,12 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp=[[-1]*(len(text2)+1) for i in range(len(text1)+1)]
-        def solve(ind1,ind2):
-            if ind1<0 or ind2<0:
-                dp[ind1][ind2]=0
-                return 0
-            if dp[ind1][ind2]!=-1:
-                return dp[ind1][ind2]
-            elif text1[ind1]==text2[ind2]:
-                dp[ind1][ind2]=1+solve(ind1-1,ind2-1)
-                return 1+solve(ind1-1,ind2-1)
-            else:
-                first=solve(ind1-1,ind2)
-                second=solve(ind1,ind2-1)
-                dp[ind1][ind2]=max(first,second)
-                return max(first,second)
-        return solve(len(text1)-1,len(text2)-1)
-        
+        dp=[[0]*(len(text2)+1) for i in range(len(text1)+1)]
+        for ind1 in range(len(text1)):
+            for ind2 in range(len(text2)):
+                if text1[ind1]==text2[ind2]:
+                    dp[ind1][ind2]=1+dp[ind1-1][ind2-1]
+                else:
+                    first=dp[ind1-1][ind2]
+                    second=dp[ind1][ind2-1]
+                    dp[ind1][ind2]=max(first,second)
+        return dp[len(text1)-1][len(text2)-1]
