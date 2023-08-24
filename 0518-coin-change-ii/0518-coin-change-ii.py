7adc3e931fc1015ac6c30e17577aa36077df3074
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-24 22:41:24 +0530

    Time: 340 ms (45.04%), Space: 37.2 MB (34.22%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -1,27 +1,17 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         n=len(coins)
-        dp=[[-1]*(amount+1) for i in range(n+1)]
-        def solve(ind,target):
-            if target==0:
-                dp[ind][target]=1
-                return 1
-            if ind==0:
-                if target%coins[0]==0:
-                    dp[ind][target]=1
-                    return 1
-                else:
-                    dp[ind][target]=0
-                    return 0
-            if dp[ind][target]!=-1:
-                return dp[ind][target]
-            notpick=solve(ind-1,target)
-            pick=0
-            if target>=coins[ind]:
-                pick=solve(ind,target-coins[ind])
-            dp[ind][target]=pick+notpick
-            return pick+notpick
-        return solve(n-1,amount)
-        
+        dp=[[0]*(amount+1) for i in range(n+1)]
+
+        for i in range(n+1):
+            dp[i][0]=1
+        for ind in range(1,n+1):
+            for target in range(1,amount+1):
+                notpick=dp[ind-1][target]
+                pick=0
+                if target>=coins[ind-1]:
+                    pick=dp[ind][target-coins[ind-1]]
+                dp[ind][target]=pick+notpick
+        return dp[n][amount]
         
         
