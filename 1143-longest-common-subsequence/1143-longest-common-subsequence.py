1acc3f71324a03375bf1c3df42dae6865d349d2d
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-24 23:32:09 +0530

    Time: 955 ms (35.73%), Space: 42.4 MB (35.91%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.py
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.py
@@ -1,12 +1,19 @@
 class Solution:
-    def longestCommonSubsequence(self, s1: str, s2: str) -> int:
-        x,y=len(s1),len(s2)
-        t=[[0]*(y+1) for _ in range(x+1)]
-        for i in range(1,x+1):
-            for j in range(1,y+1):
-                if s1[i-1]==s2[j-1]:
-                    t[i][j]=1+t[i-1][j-1]
-                else:
-                    t[i][j]=max(t[i-1][j],t[i][j-1])
-        return t[-1][-1]
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp=[[-1]*(len(text2)+1) for i in range(len(text1)+1)]
+        def solve(ind1,ind2):
+            if ind1<0 or ind2<0:
+                dp[ind1][ind2]=0
+                return 0
+            if dp[ind1][ind2]!=-1:
+                return dp[ind1][ind2]
+            elif text1[ind1]==text2[ind2]:
+                dp[ind1][ind2]=1+solve(ind1-1,ind2-1)
+                return 1+solve(ind1-1,ind2-1)
+            else:
+                first=solve(ind1-1,ind2)
+                second=solve(ind1,ind2-1)
+                dp[ind1][ind2]=max(first,second)
+                return max(first,second)
+        return solve(len(text1)-1,len(text2)-1)
         
