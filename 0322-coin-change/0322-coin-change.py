0f7ad68693aa0763e075a245f0e53cc8a551125e
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-24 15:16:36 +0530

    Time: 1795 ms (10.18%), Space: 17.2 MB (35.31%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -1,21 +1,23 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n=len(coins)
-        dp=[[0]*(amount+1) for i in range(n+1)]
+        prev=[0]*(amount+1)
         for i in range(amount+1):
             if i%coins[0]==0:
-                dp[0][i]=i//coins[0]
+                prev[i]=i//coins[0]
             else:
-                dp[0][i]=float('inf')
+                prev[i]=float('inf')
         for ind in range(1,n+1):
+            cur=[0]*(amount+1)
             for target in range(1,amount+1):
-                notpick=dp[ind-1][target]
+                notpick=prev[target]
                 take=float('inf')
                 if coins[ind-1]<=target:
-                    take=1+dp[ind][target-coins[ind-1]]
-                dp[ind][target]=min(take,notpick)
-        if dp[n][amount]==float('inf'):
+                    take=1+cur[target-coins[ind-1]]
+                cur[target]=min(take,notpick)
+            prev=cur
+        if prev[amount]==float('inf'):
             return -1
-        return dp[n][amount]
+        return prev[amount]
         
         
