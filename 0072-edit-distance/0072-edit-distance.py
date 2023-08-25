cd76a0b1c4d7a35f240d1eff1340aea7c5bc3243
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-25 13:29:38 +0530

    Time: 113 ms (77.13%), Space: 16.2 MB (99.85%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,20 +1,23 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp=[[0]*(len(word2)+1) for i in range(len(word1)+1)]
-        for i in range(len(word1)+1):
-            dp[i][0]=i
+        prev=[0]*(len(word2)+1)
+        cur=[0]*(len(word2)+1)
         for j in range(len(word2)+1):
-            dp[0][j]=j
+            prev[j]=j
         for ind1 in range(1,len(word1)+1):
+            cur=[0]*(len(word2)+1)
+            cur[0]=ind1
             for ind2 in range(1,len(word2)+1):
+                
                 if word1[ind1-1]==word2[ind2-1]:
-                    dp[ind1][ind2]=dp[ind1-1][ind2-1]
+                    cur[ind2]=prev[ind2-1]
                 else:
-                    insert=dp[ind1-1][ind2]
-                    delete=dp[ind1][ind2-1]
-                    replace=dp[ind1-1][ind2-1]
-                    dp[ind1][ind2]=1+min(insert,delete,replace)
-        return dp[len(word1)][len(word2)]
+                    insert=prev[ind2]
+                    delete=cur[ind2-1]
+                    replace=prev[ind2-1]
+                    cur[ind2]=1+min(insert,delete,replace)
+            prev=cur
+        return prev[len(word2)]
         # def solve(ind1,ind2):
         #     if ind1==0:
         #         return ind2
