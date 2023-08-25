8318029b377b3216bc3323d25ff93ce785e7a628
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-25 15:40:28 +0530

    Time: 598 ms (51.80%), Space: 16.4 MB (88.81%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,24 +1,23 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        dp=[[False]*(len(p)+1)for i in range(len(s)+1)]
+        prev=[False]*(len(p)+1)
         m=len(s)
         n=len(p)
-        dp[0][0]=True
+        prev[0]=True
         for ind2 in range(1,len(p)+1):
             flag=True
             for i in range(ind2):
                 if p[i]!="*":
                     flag=False
-                    break
-            dp[0][ind2]=flag        
+            prev[ind2]=flag
         for ind1 in range(1,len(s)+1):
+            cur=[False]*(n+1)
             for ind2 in range(1,n+1):
                 if s[ind1-1]==p[ind2-1] or p[ind2-1]=='?':
-                    dp[ind1][ind2]=dp[ind1-1][ind2-1]
+                    cur[ind2]=prev[ind2-1]
                 if p[ind2-1]=="*":
-                    first=dp[ind1-1][ind2]
-                    second=dp[ind1][ind2-1]
-                    dp[ind1][ind2]=first or second
-        return dp[m][n]
-
-        
+                    first=prev[ind2]
+                    second=cur[ind2-1]
+                    cur[ind2]=first or second
+            prev=cur
+        return prev[n]
