f75e201e481a74fbd2a042e8cf6ea5dacc7cf66d
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-25 13:07:13 +0530

    Time: 71 ms (97.72%), Space: 19.4 MB (71.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -2,13 +2,13 @@ class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         dp=[[-1]*(len(word2)+1) for i in range(len(word1)+1)]
         def solve(ind1,ind2):
-            if ind1<0:
-                return ind2+1
-            if ind2<0:
-                return ind1+1
+            if ind1==0:
+                return ind2
+            if ind2==0:
+                return ind1
             if dp[ind1][ind2]!=-1:
                 return dp[ind1][ind2]
-            if word1[ind1]==word2[ind2]:
+            if word1[ind1-1]==word2[ind2-1]:
                 dp[ind1][ind2]=solve(ind1-1,ind2-1)
                 return solve(ind1-1,ind2-1)
             insert=solve(ind1-1,ind2)
@@ -16,4 +16,4 @@ class Solution:
             replace=solve(ind1-1,ind2-1)
             dp[ind1][ind2]=1+min(insert,delete,replace)
             return 1+min(insert,delete,replace)        
-        return solve(len(word1)-1,len(word2)-1)
+        return solve(len(word1),len(word2))
