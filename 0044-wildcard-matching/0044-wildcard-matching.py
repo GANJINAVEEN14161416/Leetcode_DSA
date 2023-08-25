2ab27ccf685e7b7ba0f72a35616d3d779ba503b1
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-25 14:54:31 +0530

    Time: 609 ms (49.55%), Space: 26.7 MB (37.17%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        dp=[[-1]*(len(p)+1)for i in range(len(s)+1)]
+        def solve(ind1,ind2):
+            if ind2<0 and ind1<0:
+                return True
+            if ind2>=0 and ind1<0:
+                for i in range(ind2+1):
+                    if p[i]!="*":
+                        return False
+                return True
+            if ind1>=0 and ind2<0:
+                return False
+            if dp[ind1][ind2]!=-1:
+                return dp[ind1][ind2]
+            if s[ind1]==p[ind2] or p[ind2]=='?':
+                dp[ind1][ind2]=solve(ind1-1,ind2-1)
+                return solve(ind1-1,ind2-1)
+            if p[ind2]=="*":
+                first=solve(ind1-1,ind2)
+                second=solve(ind1,ind2-1)
+                dp[ind1][ind2]=first or second
+                return first or second
+        return solve(len(s)-1,len(p)-1)
+        
