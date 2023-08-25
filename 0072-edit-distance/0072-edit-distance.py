e33c21b014eca31fd9827985b38d89a7acb16b3d
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-25 13:21:08 +0530

    Time: 128 ms (60.92%), Space: 19.9 MB (67.85%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,19 +1,33 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp=[[-1]*(len(word2)+1) for i in range(len(word1)+1)]
-        def solve(ind1,ind2):
-            if ind1==0:
-                return ind2
-            if ind2==0:
-                return ind1
-            if dp[ind1][ind2]!=-1:
-                return dp[ind1][ind2]
-            if word1[ind1-1]==word2[ind2-1]:
-                dp[ind1][ind2]=solve(ind1-1,ind2-1)
-                return solve(ind1-1,ind2-1)
-            insert=solve(ind1-1,ind2)
-            delete=solve(ind1,ind2-1)
-            replace=solve(ind1-1,ind2-1)
-            dp[ind1][ind2]=1+min(insert,delete,replace)
-            return 1+min(insert,delete,replace)        
-        return solve(len(word1),len(word2))
+        dp=[[0]*(len(word2)+1) for i in range(len(word1)+1)]
+        for i in range(len(word1)+1):
+            dp[i][0]=i
+        for j in range(len(word2)+1):
+            dp[0][j]=j
+        for ind1 in range(1,len(word1)+1):
+            for ind2 in range(1,len(word2)+1):
+                if word1[ind1-1]==word2[ind2-1]:
+                    dp[ind1][ind2]=dp[ind1-1][ind2-1]
+                else:
+                    insert=dp[ind1-1][ind2]
+                    delete=dp[ind1][ind2-1]
+                    replace=dp[ind1-1][ind2-1]
+                    dp[ind1][ind2]=1+min(insert,delete,replace)
+        return dp[len(word1)][len(word2)]
+        # def solve(ind1,ind2):
+        #     if ind1==0:
+        #         return ind2
+        #     if ind2==0:
+        #         return ind1
+        #     if dp[ind1][ind2]!=-1:
+        #         return dp[ind1][ind2]
+        #     if word1[ind1-1]==word2[ind2-1]:
+        #         dp[ind1][ind2]=solve(ind1-1,ind2-1)
+        #         return solve(ind1-1,ind2-1)
+        #     insert=solve(ind1-1,ind2)
+        #     delete=solve(ind1,ind2-1)
+        #     replace=solve(ind1-1,ind2-1)
+        #     dp[ind1][ind2]=1+min(insert,delete,replace)
+        #     return 1+min(insert,delete,replace)        
+        # return solve(len(word1),len(word2))
