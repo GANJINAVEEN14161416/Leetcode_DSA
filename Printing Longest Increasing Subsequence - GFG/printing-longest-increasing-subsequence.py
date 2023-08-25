28d6458b73dfd876df290dd400502208b00c63ac
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-25 22:34:58 +0530

    Added solution - LeetHub

--- Printing Longest Increasing Subsequence - GFG/printing-longest-increasing-subsequence.py
+++ Printing Longest Increasing Subsequence - GFG/printing-longest-increasing-subsequence.py
@@ -7,29 +7,31 @@
 
 class Solution:
     def longestIncreasingSubsequence(self, n, arr):
-        lis=[1]*(n)
+        dp=[1]*(n)
         hash=[0]*n
-        lastind=0
+        maxind=0
         mx=1
         for i in range(n):
             hash[i]=i
             for j in range(0,i):
-                if arr[i]>arr[j] and lis[i]<1+lis[j]:
-                    lis[i]=1+lis[j]
+                if arr[i]>arr[j] and 1+dp[j]>dp[i]:
+                    dp[i]=1+dp[j]
                     hash[i]=j
-            if lis[i]>mx:
-                mx=lis[i]
-                lastind=i
+            if dp[i]>mx:
+                mx=dp[i]
+                maxind=i
         
         temp=[]
-        while hash[lastind]!=lastind:
-            temp.append(arr[lastind])
-            lastind=hash[lastind]
-        temp.append(arr[lastind])
+        while hash[maxind]!=maxind:
+            temp.append(arr[maxind])
+            maxind=hash[maxind]
+        temp.append(arr[maxind])
         return temp[::-1]
         
         
         
+
+
 
 #{ 
  # Driver Code Starts.
