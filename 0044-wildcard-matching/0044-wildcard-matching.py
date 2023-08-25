7cf90a56cf1166828e5b05c5ae9f61b2ea04cc6a
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-25 15:11:04 +0530

    Time: 660 ms (42.86%), Space: 24.8 MB (59.74%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.py
+++ 0044-wildcard-matching/0044-wildcard-matching.py
@@ -1,25 +1,43 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        dp=[[-1]*(len(p)+1)for i in range(len(s)+1)]
-        def solve(ind1,ind2):
-            if ind2==0 and ind1==0:
-                return True
-            if ind2>0 and ind1==0:
-                for i in range(ind2):
-                    if p[i]!="*":
-                        return False
-                return True
-            if ind1>0 and ind2==0:
-                return False
-            if dp[ind1][ind2]!=-1:
-                return dp[ind1][ind2]
-            if s[ind1-1]==p[ind2-1] or p[ind2-1]=='?':
-                dp[ind1][ind2]=solve(ind1-1,ind2-1)
-                return solve(ind1-1,ind2-1)
-            if p[ind2-1]=="*":
-                first=solve(ind1-1,ind2)
-                second=solve(ind1,ind2-1)
-                dp[ind1][ind2]=first or second
-                return first or second
-        return solve(len(s),len(p))
+        dp=[[False]*(len(p)+1)for i in range(len(s)+1)]
+        m=len(s)
+        n=len(p)
+        dp[0][0]=True
+        for ind2 in range(1,len(p)+1):
+            flag=True
+            for i in range(ind2):
+                if p[i]!="*":
+                    flag=False
+            dp[0][ind2]=flag
+                        
+        for ind1 in range(1,len(s)+1):
+            for ind2 in range(1,n+1):
+                if s[ind1-1]==p[ind2-1] or p[ind2-1]=='?':
+                    dp[ind1][ind2]=dp[ind1-1][ind2-1]
+                if p[ind2-1]=="*":
+                    first=dp[ind1-1][ind2]
+                    second=dp[ind1][ind2-1]
+                    dp[ind1][ind2]=first or second
+        return dp[m][n]
+#             dp=[[-1]*(len(p)+1)for i in range(len(s)+1)]
+        
+#             if ind2>0 and ind1==0:
+#                 for i in range(ind2):
+#                     if p[i]!="*":
+#                         return False
+#                 return True
+#             if ind1>0 and ind2==0:
+#                 return False
+#             if dp[ind1][ind2]!=-1:
+#                 return dp[ind1][ind2]
+#             if s[ind1-1]==p[ind2-1] or p[ind2-1]=='?':
+#                 dp[ind1][ind2]=solve(ind1-1,ind2-1)
+#                 return solve(ind1-1,ind2-1)
+#             if p[ind2-1]=="*":
+#                 first=solve(ind1-1,ind2)
+#                 second=solve(ind1,ind2-1)
+#                 dp[ind1][ind2]=first or second
+#                 return first or second
+#         return solve(len(s),len(p))
         
