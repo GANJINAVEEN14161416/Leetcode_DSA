a4d91fe919b138ac9cd092d9b00c4a8839234dc8
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-27 00:39:41 +0530

    Time: 88 ms (8.70%), Space: 19.5 MB (5.98%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,18 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        dp=[[-1]*2 for i in range(len(prices))]
-        def solve(ind,buy):
-            if ind==len(prices):
-                return 0
-            if dp[ind][buy]!=-1:
-                return dp[ind][buy]
-            if buy:
-                profit=max(-prices[ind]+solve(ind+1,0),solve(ind+1,1))
-            else:
-                profit=max(prices[ind]+solve(ind+1,1),solve(ind+1,0))
-            dp[ind][buy]=profit
-            return profit
-        return solve(0,1)
+        dp=[[0]*2 for i in range(len(prices)+1)]
+        n=len(prices)
+        dp[n][0]=dp[n][1]=0
+        for ind in range(n-1,-1,-1):
+            for buy in range(2):
+                if buy:
+                    profit=max(-prices[ind]+dp[ind+1][0],dp[ind+1][1])
+                else:
+                    profit=max(prices[ind]+dp[ind+1][1],dp[ind+1][0])
+                dp[ind][buy]=profit
+        return dp[0][1]
             
             
                 
