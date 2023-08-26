9ad01b3d991fbf9232d41fc7a4848067010c960b
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-27 02:45:27 +0530

    Time: 143 ms (61.14%), Space: 21 MB (45.41%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,18 +1,26 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         n=len(prices)
-        dp=[[-1]*2*k for i in range(n)]
-        def solve(ind,trans):
-            if ind==len(prices) or trans==2*k:
-                return 0
-            if dp[ind][trans]!=-1:
-                return dp[ind][trans]
-            if trans%2==0:
-                profit=max(-prices[ind]+solve(ind+1,trans+1),solve(ind+1,trans))
-            else:
+        dp=[[0]*(2*k+1) for i in range(n+1)]
+        for ind in range(n-1,-1,-1):
+            for trans in range(2*k-1,-1,-1):
+                if trans%2==0:
+                    profit=max(-prices[ind]+dp[ind+1][trans+1],dp[ind+1][trans])
+                else:
+                    profit=max(prices[ind]+dp[ind+1][trans+1],dp[ind+1][trans])
+                dp[ind][trans]=profit
+        return dp[0][0]
+#         def solve(ind,trans):
+#             if ind==len(prices) or trans==2*k:
+#                 return 0
+#             if dp[ind][trans]!=-1:
+#                 return dp[ind][trans]
+#             if trans%2==0:
+#                 profit=max(-prices[ind]+solve(ind+1,trans+1),solve(ind+1,trans))
+#             else:
                 
-                           profit=max(prices[ind]+solve(ind+1,trans+1),solve(ind+1,trans))
-            dp[ind][trans]=profit
-            return profit
-        return solve(0,0)
+#                            profit=max(prices[ind]+solve(ind+1,trans+1),solve(ind+1,trans))
+#             dp[ind][trans]=profit
+#             return profit
+#         return solve(0,0)
         
