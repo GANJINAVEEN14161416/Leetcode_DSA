ff3d31eb74ad7c64b85b67380332f7e9e0911812
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-27 03:18:57 +0530

    Time: 684 ms (59.39%), Space: 23.7 MB (78.42%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -6,9 +6,9 @@ class Solution:
             cur=[0]*2
             for buy in range(2):
                 if buy:
-                    profit=max(-prices[ind]+nxt[0],nxt[1])
+                    profit=max(-prices[ind]-fee+nxt[0],nxt[1])
                 else:
-                    profit=max(prices[ind]-fee+nxt[1],nxt[0])
+                    profit=max(prices[ind]+nxt[1],nxt[0])
                 cur[buy]=profit
             nxt=cur
         return cur[1]
