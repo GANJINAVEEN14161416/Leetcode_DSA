42ff7d927e882d6fc5f006040ded426d513900c6
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-08-27 01:46:32 +0530

    Time: 2093 ms (24.73%), Space: 56.7 MB (24.70%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.py
@@ -1,20 +1,15 @@
 class Solution:
     def maxProfit(self, A: List[int]) -> int:
         n=len(A)
-        dp=[[[-1 for i in range(3)] for j in range(2)] for k in range(n)]
-        # for i in range(n+1):
-        #     dp[i][0][n]
-        def solve(ind,buy,cap):
-            if cap==0 or ind==len(A):
-                return 0
-            if dp[ind][buy][cap]!=-1:
-                return dp[ind][buy][cap]
-            if buy:
-                profit=max(-A[ind]+solve(ind+1,0,cap),solve(ind+1,1,cap))
-            else:
-                profit=max(A[ind]+solve(ind+1,1,cap-1),solve(ind+1,0,cap))
-            dp[ind][buy][cap]=profit
-            return profit
-        return solve(0,1,2)
+        dp=[[[0 for i in range(3)] for j in range(2)] for k in range(n+1)]
+        for ind in range(n-1,-1,-1):
+            for buy in range(2):
+                for cap in range(2):
+                    if buy:
+                        profit=max(-A[ind]+dp[ind+1][0][cap],dp[ind+1][1][cap])
+                    else:
+                        profit=max(A[ind]+dp[ind+1][1][cap-1],dp[ind+1][0][cap])
+                    dp[ind][buy][cap]=profit
+        return dp[0][1][1]
             
             
