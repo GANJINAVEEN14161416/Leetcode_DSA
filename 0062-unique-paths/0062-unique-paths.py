ce291f9885919a7ba6c149ba7b65f2e33517003f
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-09-03 10:37:33 +0530

    Time: 37 ms (79.85%), Space: 16.5 MB (11.21%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,29 +1,15 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        prev =[0]*(n+1)
-        cur=[0]*(n+1)
-        for ind1 in range(1,m+1):
-            for ind2 in range(1,n+1):
-                if ind1==1 and ind2==1:
-                    cur[1]=1
-                else:
-                    left=0
-                    down=0
-                    if ind1-1>=0:
-                        left=prev[ind2]
-                    if ind2-1>=0:
-                        down=cur[ind2-1]
-                    cur[ind2]=left+down
-            prev=cur
-        return prev[n]
-            
-        # def solve(ind1,ind2):
-        #     if ind1==1 and ind2==1:
-        #         return 1
-        #     if ind1==0  or ind2==0:
-        #         return 0
-        #     if dp[ind1][ind2]!=-1:
-        #         return dp[ind1][ind2]
-        #     dp[ind1][ind2]=solve(ind1-1,ind2)+solve(ind1,ind2-1)
-        #     return dp[ind1][ind2]
-        # return solve(m,n)
+        dp=[[-1]*(n+1) for i in range(m)]
+        def solve(ind1,ind2):
+            if ind1==0 and ind2==0:
+                return 1
+            if ind1<0 or ind2<0:
+                return 0
+            if dp[ind1][ind2]!=-1:
+                return dp[ind1][ind2]
+            left=solve(ind1,ind2-1)
+            up=solve(ind1-1,ind2)
+            dp[ind1][ind2]=up+left
+            return left+up
+        return solve(m-1,n-1)
