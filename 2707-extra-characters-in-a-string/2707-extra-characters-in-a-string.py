5f3e94598e7c31b0a3b9f73085defa5fb1c17507
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-09-03 11:42:11 +0530

    Time: 231 ms (76.42%), Space: 16.8 MB (41.19%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -8,10 +8,10 @@ class Solution:
             # To count this character as a left over character 
             # move to index 'start + 1'
             ans = dp(start + 1) + 1
-            for end in range(start, n):
-                curr = s[start: end + 1]
+            for end in range(start, n+1):
+                curr = s[start: end]
                 if curr in dictionary_set:
-                    ans = min(ans, dp(end + 1))
+                    ans = min(ans, dp(end))
             return ans
             
         return dp(0)
