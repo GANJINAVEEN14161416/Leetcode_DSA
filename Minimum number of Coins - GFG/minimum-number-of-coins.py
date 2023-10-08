87089da658001f554df8925439f143d2c9936566
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-10-08 20:38:06 +0530

    Added solution - LeetHub

--- Minimum number of Coins - GFG/minimum-number-of-coins.py
+++ Minimum number of Coins - GFG/minimum-number-of-coins.py
@@ -3,14 +3,14 @@
 class Solution:
     def minPartition(self, N):
         # code here
-        money=[1, 2, 5, 10, 20, 50, 100, 200, 500, 2000]
+        listofCoins=[1,2,5,10,20,50,100,200,500,2000]
         ans=[]
-        for coin in range(len(money)-1,-1,-1):
-            k=N//money[coin]
-            if k>0:
-                for i in range(k):
-                    ans.append(money[coin])
-                N=N%money[coin]
+        while(N):
+            for i in range(len(listofCoins)-1,-1,-1):
+                if listofCoins[i]<=N:
+                    ans.append(listofCoins[i])
+                    N-=listofCoins[i]
+                    break
         return ans
 
 
