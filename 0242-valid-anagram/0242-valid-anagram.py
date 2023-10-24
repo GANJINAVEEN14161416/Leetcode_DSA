03dbd4c062d2f5bb2647b702562a9b76566d3080
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-10-24 19:26:43 +0530

    Time: 49 ms (80.35%), Space: 16.6 MB (99.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,10 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        dic1=defaultdict(int)
-        dic2=defaultdict(int)
-        for i in range(len(s)):
-            dic1[s[i]]+=1
-        for i in range(len(t)):
-            dic2[t[i]]+=1
-        return dic1==dic2
+        return Counter(s)==Counter(t)
         
