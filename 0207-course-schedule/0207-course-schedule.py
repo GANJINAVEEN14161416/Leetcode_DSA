7f248e072767cab3007d669d897f8fa211e7f6dd
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-10-25 23:30:40 +0530

    Time: 98 ms (61.53%), Space: 17.4 MB (99.66%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -3,26 +3,21 @@ class Solution:
         adj=defaultdict(list)
         for i in range(len(prerequisites)):
             adj[prerequisites[i][1]].append(prerequisites[i][0])
-        print(adj)
-        V=len(adj)
         q=deque()
         indegree=[0]*numCourses
         for i in range(numCourses):
             for j in adj[i]:
                 indegree[j]+=1
-        print(indegree)
         for i in range(numCourses):
             if indegree[i]==0:
                 q.append(i)
-        count=0
+        cnt=0
         while q:
-            x=q.popleft()
-            count+=1
-            print(x)
-            for child in adj[x]:
-                indegree[child]-=1
-                if indegree[child]==0:
-                    q.append(child)
-        return count==numCourses
-                
+            n=q.popleft()
+            cnt+=1
+            for i in adj[n]:
+                indegree[i]-=1
+                if indegree[i]==0:
+                    q.append(i)
+        return cnt==numCourses
         
