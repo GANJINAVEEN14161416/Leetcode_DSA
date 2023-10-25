3c8a8059d1c595e2af399e44eac0fcb1f68fc5de
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-10-25 15:48:46 +0530

    Time: 141 ms (43.66%), Space: 17.3 MB (99.94%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.py
+++ 0130-surrounded-regions/0130-surrounded-regions.py
@@ -3,29 +3,26 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        q=deque()         
-        n=len(board)
-        m=len(board[0])
-        visit=[[False]*m for i in range(n)]
-        for i in range(n):
-            for j in range(m):
-                if (i==0 or j==0 or j==m-1 or i==n-1) and (board[i][j]=="O"):
+        m=len(board)
+        n=len(board[0])
+        q=deque()
+        vis=[[False]*n for i in range(m)]
+        for i in range(m):
+            for j in range(n):
+                if (i==0 or j==n-1 or i==m-1 or j==0) and board[i][j]=="O":
                     q.append([i,j])
-                    visit[i][j]=True
-        m1=[0,-1,0,1]
-        m2=[1,0,-1,0]
+                    vis[i][j]=True
+        fourdirections=[[1,0],[0,1],[-1,0],[0,-1]]
         while q:
-            r,c=q.popleft()
-            for new in range(4):
-                newrow=r+m1[new]
-                newcol=c+m2[new]
-                if newrow>=0 and newrow<n and newcol>=0 and newcol<m and board[newrow][newcol]=="O" and not visit[newrow][newcol]:
-                    visit[newrow][newcol]=True
+            x,y=q.popleft()
+            for i,j in fourdirections:
+                newrow=x+i
+                newcol=y+j
+                if newrow>=0 and newrow<m and newcol>=0 and newcol<n and board[newrow][newcol]=="O" and not vis[newrow][newcol]:
                     q.append([newrow,newcol])
-        for i in range(n):
-            for j in range(m):
-                if not visit[i][j] and board[i][j]=="O":
+                    vis[newrow][newcol]=True
+        for i in range(m):
+            for j in range(n):
+                if not vis[i][j]:
                     board[i][j]="X"
         return board
-        
-        
