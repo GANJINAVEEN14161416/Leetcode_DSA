1398575e7adb91ec874ed2621fc38299a542112f
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-10-25 19:26:14 +0530

    Added solution - LeetHub

--- Number of Distinct Islands - GFG/number-of-distinct-islands.py
+++ Number of Distinct Islands - GFG/number-of-distinct-islands.py
@@ -5,29 +5,29 @@ from typing import List
 sys.setrecursionlimit(10**8)
 class Solution:
     def countDistinctIslands(self, grid : List[List[int]]) -> int:
-        def dfs(i,j,i0,j0,list1):
+        # code here
+        m=len(grid)
+        n=len(grid[0])
+        visit=[[False]*n for i  in range(m)]
+        four_direction=[[0,1],[1,0],[-1,0],[0,-1]]
+        def dfs(i,j,x,y,list1):
             visit[i][j]=True
-            list1.append((i-i0,j-j0))
-            for change in range(4):
-                i1=i+m1[change]
-                j1=j+m2[change]
-                if i1>=0 and i1<len(grid) and j1>=0 and j1<len(grid[0]) and not visit[i1][j1] and grid[i1][j1]==1:
-                    dfs(i1,j1,i0,j0,list1)
-        m1=[0,1,0,-1]
-        m2=[1,0,-1,0]
-        count=0
-        row=len(grid)
-        col=len(grid[0])
+            list1.append((x-i,y-j))
+            for a,b in four_direction:
+                newrow=i+a
+                newcol=j+b
+                if newrow>=0 and newrow<m and newcol>=0 and newcol<n and not visit[newrow][newcol] and grid[newrow][newcol]==1:
+                    visit[newrow][newcol]=True
+                    dfs(newrow,newcol,x,y,list1)
         s=set()
-        visit=[[False]*col for i in range(row)]
-        for i in range(row):
-            for j in range(col):
-                if not visit[i][j] and grid[i][j]==1:
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j]==1 and not visit[i][j]:
                     list1=[]
                     dfs(i,j,i,j,list1)
                     s.add(tuple(list1))
         return len(s)
-
+                    
 
 
 #{ 
