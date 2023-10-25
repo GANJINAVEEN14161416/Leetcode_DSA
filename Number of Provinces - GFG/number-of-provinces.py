5e5208aa2d6c7c18be489999912d4550d16d8c45
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-10-25 12:22:05 +0530

    Added solution - LeetHub

--- Number of Provinces - GFG/number-of-provinces.py
+++ Number of Provinces - GFG/number-of-provinces.py
@@ -1,26 +1,33 @@
 #User function Template for python3
-
+from collections import *
 class Solution:
     def numProvinces(self, adj, V):
         # code here 
-        ad=[[] for i in range(V+1)]
+        graph=[[] for i in range(V+1)]
         for i in range(V):
             for j in range(V):
                 if i!=j and adj[i][j]==1:
-                    ad[i+1].append(j+1)
-        vis=[False]*(V+1)
-        ans=[]
+                    graph[i+1].append(j+1)
+
         count=0
-        def dfs(child):
-            vis[child]=True
-            for w in ad[child]:
-                if not vis[w]:
-                    dfs(w)
+        vis=[False]*(V+1)
+        def bfs(i):
+            q=deque()
+            q.append(i)
+            while q:
+                pop=q.popleft()
+                for j in graph[pop]:
+                    if not vis[j]:
+                        q.append(j)
+                        vis[j]=True
+    
         for i in range(1,V+1):
             if not vis[i]:
                 count+=1
-                dfs(i)
+                vis[i]=True
+                bfs(i)
         return count
+
 
 #{ 
  # Driver Code Starts
