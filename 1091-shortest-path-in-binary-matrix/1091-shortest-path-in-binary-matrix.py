4154633a7cc099c31a7cfcc6f0d5d462aa2a87fb
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-10-26 09:43:28 +0530

    Time: 478 ms (87.37%), Space: 16.6 MB (93.72%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,33 +1,28 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
+        m=len(grid)
+        n=len(grid[0])
+        eight_direction=[[-1,1],[1,1],[-1,-1],[1,-1],[0,1],[1,0],[-1,0],[0,-1]]
+        if grid[0][0]!=0:
+            return -1
         q=deque()
-        r1,c1=0,0
-        if grid[0][0]==1:
-            return -1
-        q.append([1,r1,c1])
-        m1=[[0,1],[-1,1],[-1,0],[-1,-1],[0,-1],[1,-1],[1,0],[1,1]]
-        rowl=len(grid)
-        coll=len(grid[0])
-        visit=[[False]*coll for i in range(rowl)]
-        visit[r1][c1]=True
-        r2=len(grid)-1
-        c2=len(grid)-1
+        q.append([0,0,1])
+        d1=m-1
+        d2=n-1
+        visit=[[False]*n for i in range(m)]
+        visit[0][0]=True
         while q:
-            dis,r,c=q.popleft()
-            if r==r2 and c==c2 and visit[r][c]:
-                return dis
-                break
-            for new in range(8):
-                row=m1[new][0]+r
-                col=m1[new][1]+c
-                if row>=0 and row<rowl and col>=0 and col<coll and grid[row][col]==0 and not visit[row][col]:
-                        print(row,col)
-                        visit[row][col]=True
-                        q.append([dis+1,row,col])
-        if visit[r2][c2]==False:
+            x,y,steps=q.popleft()
+            if visit[d1][d2]==True and x==d1 and y==d2:
+                return steps
+            for r,c in eight_direction:
+                newrow=r+x
+                newcol=c+y
+                if newrow>=0 and newcol>=0 and newrow<m and newcol<n and grid[newrow][newcol]==0 and not visit[newrow][newcol]:
+                    q.append([newrow,newcol,steps+1])
+                    visit[newrow][newcol]=True
+        if visit[d1][d2]==False:
             return -1
-        else:
-            return dis
-        
-        
-        
+        return steps
+                    
+            
