43bdc03664c6903f7b0785bfb647609b345bcf79
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-01 19:49:19 +0530

    Time: 8059 ms (5.01%), Space: 17.8 MB (67.53%) - LeetHub

--- 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.py
@@ -1,21 +1,23 @@
 class Solution:
-    def minimumEffortPath(self, grid: List[List[int]]) -> int:
-        m=len(grid)
-        n=len(grid[0])
-        q=[]
-        four_direction=[[1,0],[0,1],[-1,0],[0,-1]]
-        heapq.heappush(q,[0,0,0])
+    def minimumEffortPath(self, heights: List[List[int]]) -> int:
+        m=len(heights)
+        n=len(heights[0])
         distance=[[float('inf')]*n for i in range(m)]
+        four_direction=[[1,0],[0,1],[-1,0],[0,-1]]
+        q=deque()
+        q.append([0,0,0])
+        l1,l2=m-1,n-1
+        efforts=0
         distance[0][0]=0
         while q:
-            diff,x,y=heapq.heappop(q)
-            for r,c in four_direction:
-                newrow=x+r
-                newcol=y+c
-                if newrow>=0 and newcol>=0 and newrow<m and newcol<n:
-                    maxi_effort=max(diff,abs(grid[newrow][newcol]-grid[x][y]))
-                    if maxi_effort<distance[newrow][newcol]:
-                        heapq.heappush(q,[maxi_effort,newrow,newcol])
-                        distance[newrow][newcol]=maxi_effort
+            r,c,diff=q.popleft()
+            for x,y in four_direction:
+                newrow=r+x
+                newcol=c+y
+                if newrow>=0 and newrow<m and newcol>=0 and newcol<n:
+                    efforts=max(diff,abs(heights[r][c]-heights[newrow][newcol]))
+                    if efforts<distance[newrow][newcol]:
+                        distance[newrow][newcol]=efforts
+                        q.append([newrow,newcol,efforts])
         return distance[-1][-1]
         
