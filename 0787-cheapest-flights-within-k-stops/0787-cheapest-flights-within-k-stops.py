22e5c93f44cfa39665ff9e795661708fcaaec195
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-02 11:50:11 +0530

    Time: 105 ms (72.63%), Space: 17.6 MB (48.45%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.py
@@ -1,29 +1,21 @@
 class Solution:
-    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], sr: int, dst: int, k: int) -> int:
         adj=defaultdict(list)
-        for innode,destnode,wt in flights:
-            adj[innode].append([destnode,wt])
+        for u,v, w in flights:
+            adj[u].append([v,w])
         q=deque()
+        steps=0
+        q.append([0,sr,steps])
         distance=[float('inf')]*n
-#         q=deque()
-#         heapq.heappush(pq,[0,src,0])
-#         distance[src]=0
-#         while pq:
-#             steps,dis,node=heapq.heappop(pq)
-#             if dst==node:
-#                 distance[dst]
-#             for child,wt in adj[node]:
-#                 if dis+wt<distance[child] and steps<=k:
-#                     distance[child]=dis+wt
-#                     heapq.heappush(pq,[steps+1,dis+wt,child])
-#         print(distance)
-#         return distance[dst] if distance[dst]!=float('inf') else -1
-        q=deque()
-        q.append([0,src,0])
+        distance[sr]=0
         while q:
-            dis,node,steps=q.popleft()
-            for child,wt in adj[node]:
-                if dis+wt<distance[child] and steps<=k:
-                    distance[child]=dis+wt
-                    q.append([dis+wt,child,steps+1])
-        return distance[dst] if distance[dst]!=float('inf') else -1
+            dis,s,steps=q.popleft()
+            for child,wei in adj[s]:
+                if wei+dis<distance[child] and steps<=k:
+                    distance[child]=wei+dis
+                    q.append([wei+dis,child,steps+1])
+        return -1 if distance[dst]==float('inf') else distance[dst]
+                    
+            
+            
+        
