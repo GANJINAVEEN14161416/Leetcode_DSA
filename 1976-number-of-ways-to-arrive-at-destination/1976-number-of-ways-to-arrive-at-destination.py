a815988cf42feffcb814929bc92e623b4c41c887
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-02 12:57:14 +0530

    Time: 289 ms (40.39%), Space: 22.9 MB (39.03%) - LeetHub

--- 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
+++ 1976-number-of-ways-to-arrive-at-destination/1976-number-of-ways-to-arrive-at-destination.py
@@ -1,25 +1,28 @@
 class Solution:
     def countPaths(self, n: int, roads: List[List[int]]) -> int:
+        ways=[0]*(n)
+        distance=[float('inf')]*(n)
         adj=defaultdict(list)
-        for u,v,w in roads:
+        for u,v ,w in roads:
             adj[u].append([v,w])
             adj[v].append([u,w])
-        distance=[float('inf')]*n
         q=[]
         distance[0]=0
-        ways=[0]*n
-        ways[0]=1
         heapq.heappush(q,[0,0])
+        mod=10**9+7
+        ways[0]=1
         while q:
             dis,node=heapq.heappop(q)
-            for child,wt in adj[node]:
-                if wt+dis<distance[child]:
-                    distance[child]=wt+dis
+            for child,wei in adj[node]:
+                if dis+wei<distance[child]:
+                    distance[child]=dis+wei
                     ways[child]=ways[node]
-                    heapq.heappush(q,[wt+dis,child])
-                elif wt+dis==distance[child]:
-                    ways[child]+=(ways[node])%(10**9+7)
-            print(ways)
-        return ways[-1]%(10**9+7)
+                    heapq.heappush(q,[wei+dis,child])
+                elif distance[child]==dis+wei:
+                    ways[child]+=ways[node]
+        return ways[-1]%mod
+    
                 
+        
+        
         
