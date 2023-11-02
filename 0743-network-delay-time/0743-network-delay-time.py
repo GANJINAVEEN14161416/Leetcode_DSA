cf6dcd4a8bfcc12496dcb7513ccce24985d5e7dc
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-02 12:05:49 +0530

    Time: 415 ms (61.08%), Space: 18.1 MB (98.60%) - LeetHub

--- 0743-network-delay-time/0743-network-delay-time.py
+++ 0743-network-delay-time/0743-network-delay-time.py
@@ -1,17 +1,22 @@
 class Solution:
     def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        distance=[float('inf')]*(n+1)
+        q=deque()
+        distance[0]=-1
         adj=defaultdict(list)
         for u,v,w in times:
             adj[u].append([v,w])
-        q=[]
-        heapq.heappush(q,([0,k]))
-        
-        distance=[float('inf')]*(n+1)
+        q.append([0,k])
         distance[k]=0
         while q:
-            dis,node=heapq.heappop(q)
-            for child,wt in adj[node]:
-                if wt+dis<distance[child]:
-                    distance[child]=wt+dis
-                    heapq.heappush(q,[wt+dis,child])
-        return max(distance[1:]) if max(distance[1:])!=float('inf') else -1
+            dis,node=q.popleft()
+            for child,wei in adj[node]:
+                if dis+wei<distance[child]:
+                    distance[child]=dis+wei
+                    q.append([dis+wei,child])
+        for i in distance[1:]:
+            if i==float('inf'):
+                return -1
+        return max(distance[1:])
+        
+        
