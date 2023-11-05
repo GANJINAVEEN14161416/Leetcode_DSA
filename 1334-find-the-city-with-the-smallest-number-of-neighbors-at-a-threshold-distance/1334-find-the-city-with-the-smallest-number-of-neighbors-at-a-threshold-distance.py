9b7f46e1eb83fe7977ffabffb479fd64dd77e4c8
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-05 14:50:19 +0530

    Time: 573 ms (39.22%), Space: 17.5 MB (46.14%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -1,23 +1,27 @@
 class Solution:
     def findTheCity(self, n: int, edges: List[List[int]], distanceThreshold: int) -> int:
         matrix=[[float('inf')]*n for i in range(n)]
-        for i in range(n):
-            matrix[i][i]=0
-        for u,v,w in edges:
-            matrix[u][v]=w
-            matrix[v][u]=w
+        for i,j,w in edges:
+            matrix[i][j]=matrix[j][i]=w
+        for i  in range(n):
+            for j in range(n):
+                if i==j:
+                    matrix[i][j]=0
         for k in range(n):
             for i in range(n):
                 for j in range(n):
                     matrix[i][j]=min(matrix[i][j],matrix[i][k]+matrix[k][j])
-        nei=n
         index=-1
+        ans=float('inf')
         for i in range(n):
-            count=0
+            max_city=0
             for j in range(n):
-                if matrix[i][j]<=distanceThreshold:
-                    count+=1
-            if nei>=count:
-                nei=count
-                index=i
+                if matrix[i][j]<=distanceThreshold and matrix[i][j]!=0:
+                    max_city+=1
+            if max_city<=ans:
+                ans=max_city
+                index=max(index,i)
         return index
+                    
+                
+        
