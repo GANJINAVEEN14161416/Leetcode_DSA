affd2daf7f1514a3ed63a4e4f5e3e7d6c578df59
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-06 12:11:17 +0530

    Time: 58 ms (95.83%), Space: 17.7 MB (23.92%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -2,17 +2,14 @@ class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n=len(triangle)
         dp=[[-1]*n for i in range(n)]
-        def solve(ind1,ind2):
-            if ind1==n-1:
-                dp[ind1][ind2]=triangle[n-1][ind2]
-                return triangle[n-1][ind2]
-            if dp[ind1][ind2]!=-1:
-                return dp[ind1][ind2]
-            down=triangle[ind1][ind2]+solve(ind1+1,ind2)
-            diagonal=solve(ind1+1,ind2+1)+triangle[ind1][ind2]
-            dp[ind1][ind2]=min(down,diagonal)
-            return dp[ind1][ind2]
-        return solve(0,0)
+        for i in range(n):
+            dp[n-1][i]=triangle[n-1][i]
+        for ind1 in range(n-2,-1,-1):
+            for ind2 in range(ind1+1):
+                down=triangle[ind1][ind2]+dp[ind1+1][ind2]
+                diagonal=dp[ind1+1][ind2+1]+triangle[ind1][ind2]
+                dp[ind1][ind2]=min(down,diagonal)
+        return dp[0][0]
     
             
     
