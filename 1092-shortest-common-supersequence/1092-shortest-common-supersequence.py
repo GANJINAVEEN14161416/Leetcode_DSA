56ca5e351db2b4f5e6b4a4996ad6f4119c364250
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-07 23:57:17 +0530

    Time: 414 ms (31.18%), Space: 41.5 MB (25.20%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -1,33 +1,31 @@
 class Solution:
-    def shortestCommonSupersequence(self, text1: str, text2: str) -> str:
-        dp=[[0]*(len(text2)+1) for i in range(len(text1)+1)]
-        for ind1 in range(1,len(text1)+1):
-            for ind2 in range(1,len(text2)+1):
-                if text1[ind1-1]==text2[ind2-1]:
-                    dp[ind1][ind2]=1+dp[ind1-1][ind2-1]
+    def shortestCommonSupersequence(self, s1: str, s2: str) -> str:
+        x=len(s1)
+        y=len(s2)
+        dp=[[0]*(y+1) for i in range(x+1)]
+        for ind1 in range(1,x+1):
+            for ind2 in range(1,y+1):
+                if s1[ind1-1]==s2[ind2-1]:
+                    dp[ind1][ind2]=1+ dp[ind1-1][ind2-1]
                 else:
-                    first=dp[ind1-1][ind2]
-                    second=dp[ind1][ind2-1]
-                    dp[ind1][ind2]=max(first,second)
-        ind1=len(text1)
-        ind2=len(text2)
-        ans=""
-        while ind1>0  and ind2>0:
-            if text1[ind1-1]==text2[ind2-1]:
-                ans+=text1[ind1-1]
+                    dp[ind1][ind2]=max(dp[ind1-1][ind2],dp[ind1][ind2-1])
+        ind1,ind2,ans=x,y,""
+        while ind1>0 and ind2>0:
+            if s1[ind1-1]==s2[ind2-1]:
+                ans+=s2[ind2-1]
                 ind1-=1
                 ind2-=1
-            elif dp[ind1][ind2-1]>dp[ind1-1][ind2]:
-                ans+=text2[ind2-1]
-                ind2-=1
+            elif dp[ind1-1][ind2]>dp[ind1][ind2-1]:
+                ans+=s1[ind1-1]
+                ind1-=1
             else:
-                ans+=text1[ind1-1]
-                ind1-=1
+                ans+=s2[ind2-1]
+                ind2-=1
         while ind1>0:
-            ans+=text1[ind1-1]
+            ans+=s1[ind1-1]
             ind1-=1
         while ind2>0:
-            ans+=text2[ind2-1]
+            ans+=s2[ind2-1]
             ind2-=1
         return ans[::-1]
         
