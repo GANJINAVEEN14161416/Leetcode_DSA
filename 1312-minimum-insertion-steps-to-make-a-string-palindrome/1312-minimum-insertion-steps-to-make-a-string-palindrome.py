458a2e95c884b0dd39c216bcd234a6edb23e17a6
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-07 23:39:10 +0530

    Time: 829 ms (42.96%), Space: 18.1 MB (79.56%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.py
@@ -1,15 +1,14 @@
 class Solution:
     def minInsertions(self, s: str) -> int:
-        text1=s
-        text2=s[::-1]
-        dp=[[0]*(len(text2)+1) for i in range(len(text1)+1)]
-        for ind1 in range(1,len(text1)+1):
-            for ind2 in range(1,len(text2)+1):
-                if text1[ind1-1]==text2[ind2-1]:
-                    dp[ind1][ind2]=1+dp[ind1-1][ind2-1]
+        x=y=len(s)
+        s1=s
+        s2=s[::-1]
+        dp=[[0]*(y+1) for i in range(x+1)]
+        for ind1 in range(1,x+1):
+            for ind2 in range(1,y+1):
+                if s1[ind1-1]==s2[ind2-1]:
+                    dp[ind1][ind2]=1+ dp[ind1-1][ind2-1]
                 else:
-                    first=dp[ind1-1][ind2]
-                    second=dp[ind1][ind2-1]
-                    dp[ind1][ind2]=max(first,second)
-        return len(s)-dp[len(text1)][len(text2)]
+                    dp[ind1][ind2]=max(dp[ind1-1][ind2],dp[ind1][ind2-1])
+        return x-dp[-1][-1]
         
