c427b115f9909b8bbd26fb85623c98b91a03290d
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-08 10:28:05 +0530

    Time: 455 ms (48.57%), Space: 72.7 MB (58.52%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.py
+++ 0115-distinct-subsequences/0115-distinct-subsequences.py
@@ -1,21 +1,16 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         m,n=len(s),len(t)
-        dp=[[-1]*n for i in range(m)]
-        def solve(ind1,ind2):
-            if ind2<0:
-                return 1
-            if ind1<0:
-                return 0
-            if dp[ind1][ind2]!=-1:
-                return dp[ind1][ind2]
-            if s[ind1]==t[ind2]:
-                dp[ind1][ind2]=solve(ind1-1,ind2-1)+solve(ind1-1,ind2)
-                return dp[ind1][ind2] 
-            else:
-                dp[ind1][ind2]=solve(ind1-1,ind2)
-                return dp[ind1][ind2] 
-        return solve(m-1,n-1)
+        dp=[[0]*(n+1) for i in range(m+1)]
+        for i in range(m):
+            dp[i][0]=1
+        for ind1 in range(1,m+1):
+            for ind2 in range(1,n+1):
+                if s[ind1-1]==t[ind2-1]:
+                    dp[ind1][ind2]=dp[ind1-1][ind2-1]+dp[ind1-1][ind2]
+                else:
+                    dp[ind1][ind2]=dp[ind1-1][ind2]
+        return dp[-1][-1]
         
         
         
