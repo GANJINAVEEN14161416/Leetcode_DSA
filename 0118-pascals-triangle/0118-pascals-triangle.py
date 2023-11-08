d9ab11cefab4db3ab421a5971587c182bcf2603c
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-09 00:14:22 +0530

    Time: 32 ms (94.07%), Space: 16.2 MB (83.81%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -4,7 +4,7 @@ class Solution:
         for i in range(numRows):
             temp=[]
             for j in range(i+1):
-                if j==0 or i==j:
+                if j==0 or j==i:
                     temp.append(1)
                 else:
                     temp.append(ans[i-1][j-1]+ans[i-1][j])
