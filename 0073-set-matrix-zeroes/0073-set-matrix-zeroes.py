4f05c842e36ab6be81550e552b3473a09174b75f
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-08 23:17:55 +0530

    Time: 115 ms (65.39%), Space: 17 MB (98.37%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -2,17 +2,29 @@ class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         m=len(matrix)
         n=len(matrix[0])
-        for i in range(m):
-            for j in range(n):
+        iszerorow=False
+        iszerocol=False
+        for i in range(n):# for first row
+            if matrix[0][i]==0:
+                iszerorow=True
+        for i in range(m):# for first column
+            if matrix[i][0]==0:
+                iszerocol=True
+        for i in range(1,m):
+            for j in range(1,n):
                 if matrix[i][j]==0:
-                    for col in range(n):
-                        if matrix[i][col]!=0:
-                            matrix[i][col]="#"
-                    for row in range(m):
-                        if matrix[row][j]!=0:
-                            matrix[row][j]="#"
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j]=="#":
+                    matrix[0][j]=0# make first row zero
+                    matrix[i][0]=0 # make first column zero
+        for i in range(1,m):
+            for j in range(1,n):
+                if matrix[i][0]==0 or matrix[0][j]==0:# now make zero on respective row and column
                     matrix[i][j]=0
+        if iszerorow==True:
+            for i in range(n): # make for first row
+                matrix[0][i]=0
+        if iszerocol==True:
+            for i in range(m):# make for first column
+                matrix[i][0]=0
+                
+            
         
