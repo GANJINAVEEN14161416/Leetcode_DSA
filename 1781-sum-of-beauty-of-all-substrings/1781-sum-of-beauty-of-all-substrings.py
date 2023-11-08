6ea755b5a57b08fbcfedc94d174738491165e867
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-08 15:40:30 +0530

    Time: 5960 ms (40.00%), Space: 16.4 MB (16.08%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.py
@@ -1,10 +1,17 @@
 class Solution:
     def beautySum(self, s: str) -> int:
-        n,count=len(s),0
-        for  i in range(n):
+        ans=0
+        n=len(s)
+        for i in range(n):
             freq=[0]*26
             for j in range(i,n):
                 freq[ord(s[j])-97]+=1
-                count+=max(freq)-(min(x  for x in freq if x))
-        return count
-        
+                mx=max(freq)
+                mi=float('inf')
+                for k in range(26):
+                    if freq[k]:
+                        mi=min(freq[k],mi)
+                if mi!=float('inf'):
+                    ans+=mx-mi
+        return ans
+                
