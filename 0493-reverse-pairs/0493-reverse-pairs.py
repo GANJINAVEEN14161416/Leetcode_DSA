f3402c61c3b2f250b8edbafffc4eb0eeaa637800
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-10 12:22:56 +0530

    Time: 1746 ms (47.72%), Space: 23.6 MB (86.93%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -1,37 +1,36 @@
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-        count=0
-        if len(nums)>1:
-            # calculate mid
-            mid=len(nums)//2
-            # divide the input array in to right and left
-            left=nums[:mid]
-            right=nums[mid:]
-            count+=self.reversePairs(left)
-            count+=self.reversePairs(right)
-            i=0
-            j=0
-            # the tricky part - updating the count of number of possible pairs
-            for i in range(len(left)):
-                while j<len(right) and left[i]>right[j]*2:
-                    j+=1
-                count+=j
-            # merge two sorted array
-            i=j=k=0        
-            while i<len(left) and j<len(right):
-                if left[i]>right[j]:
-                    nums[k]=right[j]
-                    j+=1
+        ans,n=[0]*1,len(nums)
+        def mergesort(nums):
+            if len(nums)>1:
+                mid=len(nums)//2
+                left_nums=nums[:mid]
+                right_nums=nums[mid:]
+                mergesort(left_nums)
+                mergesort(right_nums)
+                i=j=0
+                for i in range(len(left_nums)):
+                    while j<len(right_nums) and  left_nums[i]>2*right_nums[j] :
+                        j+=1
+                    ans[0]+=j
+                i=k=j=0
+                while i<len(left_nums) and j<len(right_nums):
+                    if left_nums[i]<right_nums[j]:
+                        nums[k]=left_nums[i]
+                        i+=1
+                    else:
+                        nums[k]=right_nums[j]
+                        j+=1
                     k+=1
-                else:
-                    nums[k]=left[i]
+                while i<len(left_nums):
+                    nums[k]=left_nums[i]
                     i+=1
                     k+=1
-            while j<len(right):
-                nums[k]=right[j]
-                j,k=j+1,k+1
-            while i<len(left):
-                nums[k]=left[i]
-                i,k=i+1,k+1     
-        return count 
-        
+                while j<len(right_nums):
+                    nums[k]=right_nums[j]
+                    j+=1
+                    k+=1
+        mergesort(nums)
+        return ans[0]
+                    
+                
