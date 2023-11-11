5f03d5f03eff8d9ff4fac64e9cf6f9a03ed74e57
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-11 17:37:36 +0530

    Added solution - LeetHub

--- Sort a stack - GFG/sort-a-stack.py
+++ Sort a stack - GFG/sort-a-stack.py
@@ -3,33 +3,55 @@ class Solution:
     # function sort the stack such that top element is max
     # funciton should return nothing
     # s is a stack
-    def sorted(self, s):
-        if not s:
-            return
-        num=s.pop()
-        self.sorted(s)
-        self.merge(s,num)
-    def merge(self,s,num):
-        if (not s) or num>=s[-1]:
-            s.append(num)
-            return 
-        temp=s.pop()
-        self.merge(s,num)
-        s.append(temp)
+    def Sorted(self, s):
+        # Code here
+        def sort1(s):
+            if len(s)==0:
+                return 
+            num=s.pop()
+            sort1(s)
+            merge(num)
+        def merge(num):
+            if not s or num>=s[-1]:
+                s.append(num)
+                return 
+            temp=s.pop()
+            merge(num)
+            s.append(temp)
+        return sort1(s)
+    # def sorted(self, s):
+    #     def merge(s):
+    #         if len(s)==0:
+    #             return 
+    #         num=s.pop()
+    #         merge(s)
+    #         mergesort(s,num)
+    #     def mergesort(s,num):
+    #         if not s or num>=s[-1]:
+    #             s.append(num)
+    #             return 
+    #         temp=s.pop()
+    #         mergesort(s,num)
+    #         s.append(temp)
+    #     return merge(s)
+                
+
+
         
-        
+            
 
 
 
 #{ 
  # Driver Code Starts
+
 if __name__=='__main__':
     t = int(input())
     for i in range(t):
         n = int(input())
         arr = list(map(int, input().strip().split()))
         ob = Solution()
-        ob.sorted(arr)
+        ob.Sorted(arr)
         for e in range(len(arr)):
             print(arr.pop(), end=" ")
         print()
