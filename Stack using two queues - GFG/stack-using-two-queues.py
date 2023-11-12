4f20bf197aefe608d9a48a6d7f2eb7b7d60f15b8
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-12 07:29:50 +0530

    Added solution - LeetHub

--- Stack using two queues - GFG/stack-using-two-queues.py
+++ Stack using two queues - GFG/stack-using-two-queues.py
@@ -1,22 +1,33 @@
+#User function Template for python3
+'''
+    :param x: value to be inserted
+    :return: None
 
-from collections import deque
-
+    queue_1 = [] # first queue
+    queue_2 = [] # second queue
+   '''
+   
+#Function to push an element into stack using two queues.
 def push(x):
     
     # global declaration
     global queue_1
     global queue_2
+    
     queue_1.append(x)
-    for i in range(len(queue_1)):
+    for i in range(len(queue_1)-1):
         queue_1.append(queue_1.pop(0))
+
+
 #Function to pop an element from stack using two queues.     
 def pop():
     
     # global declaration
     global queue_1
+    
     global queue_2
     if queue_1:
-        return queue_1.pop(-1)
+        return queue_1.pop(0)
     return -1
     # code here
 
