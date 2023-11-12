0655a2c665d01d1f8137573e7424c50b835937c2
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-11-12 08:00:57 +0530

    Added solution - LeetHub

--- Circular tour - GFG/circular-tour.py
+++ Circular tour - GFG/circular-tour.py
@@ -1,18 +1,22 @@
+'''
+    lis[][0]:Petrol
+    lis[][1]:Distance
+'''
 
 class Solution:
+    
+    #Function to find starting point where the truck can start to get through
+    #the complete circle without exhausting its petrol in between.
     def tour(self,lis, n):
-        fuel = 0
-        deficit = 0
-        start = 0
+        #Code here
+        surplus=deficit=ans=0
         for i in range(n):
-            fuel += lis[i][0] - lis[i][1]
-            if fuel < 0:
-                deficit+=fuel
-                fuel = 0
-                start = i + 1
-        return start if fuel + deficit >= 0 else -1
-            
-
+            surplus+=lis[i][0]-lis[i][1]
+            if surplus<0:
+                ans=i+1
+                deficit+=surplus
+                surplus=0
+        return ans if surplus+deficit>=0 else -1
 
 
 #{ 
