09988a9a32f01b2335cf0c02bc568152c8c88322
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-12-01 00:10:53 +0530

    Added solution - LeetHub

--- Aggressive Cows - GFG/aggressive-cows.py
+++ Aggressive Cows - GFG/aggressive-cows.py
@@ -1,28 +1,26 @@
 #User function Template for python3
 
 class Solution:
-    def solve(self,n,k,arr):
-        arr.sort()
-        low = 0 
-        high = arr[n-1]-arr[0]
-        res = []
-        while low<=high:
-            mid = low+high>>1;
-            if self.cow_Place(arr,n,k,mid):
-                res.append(mid)
-                low = mid+1
+    def solve(self,n,k,stalls):
+        left,right=1,max(stalls)
+        ans=0
+        stalls.sort()
+        def possible(mid):
+            cows=1
+            last=stalls[0]
+            for i in stalls[1:]:
+                if i-last>=mid:
+                    cows+=1
+                    last=i
+            return cows>=k
+        while left<=right:
+            mid=(left+right)//2
+            if possible(mid):
+                ans=max(mid,ans)
+                left=mid+1
             else:
-                high = mid-1;
-        return max(res)
-    def cow_Place(self , arr,n,k,mid):
-        coor = arr[0] 
-        count =1
-        for i in range(1,n):
-            if arr[i]-coor>=mid:
-                count+=1
-                coor = arr[i];
-            if count==k:return True
-        return False
+                right=mid-1
+        return ans
 
 
 #{ 
