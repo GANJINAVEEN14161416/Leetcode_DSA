f72a123e206b0c7cb8f38a2c8fa38b44a86486fd
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-12-30 14:32:09 +0530

    Time: 81 ms (5.83%), Space: 17.3 MB (10.82%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -2,13 +2,13 @@ class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
         index=-1
         n=len(nums)
-        for i in range(n-1,0,-1):
-            if nums[i]>nums[i-1]:
-                index=i-1
+        for i in range(n-2,-1,-1):
+            if nums[i+1]>nums[i]:
+                index=i
                 break
         if index==-1:
             return nums.sort()
-        for i in range(n-1,0,-1):
+        for i in range(n-1,-1,-1):
             if nums[i]>nums[index]:
                 nums[i],nums[index]=nums[index],nums[i]
                 break
@@ -18,6 +18,7 @@ class Solution:
             nums[left],nums[right]=nums[right],nums[left]
             left+=1
             right-=1
+        
         
             
         
