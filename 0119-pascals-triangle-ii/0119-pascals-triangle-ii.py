4f5c58e0f96c53af0c8327687b40510ad3a09d95
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-12-30 12:28:44 +0530

    Time: 62 ms (6.41%), Space: 17.3 MB (5.33%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -1,15 +1,14 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         ans=[]
-        def comb(r,c):
-            val=1
+        def pascal(r,c):
+            x=1
             for i in range(c):
-                val=val*(r-i)
-                val=val//(i+1)
-            return val
-        for k in range(1,rowIndex+2):
-            ans.append(comb(rowIndex,k-1))
+                x=x*(r-i)//(i+1)
+            return x
+
+        for i in range(rowIndex+1):
+            ans.append(pascal(rowIndex,i))
         return ans
-        
-        
-        
+                
+                
