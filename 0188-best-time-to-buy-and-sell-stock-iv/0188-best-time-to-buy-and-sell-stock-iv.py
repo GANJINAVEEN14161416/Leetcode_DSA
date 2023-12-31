84e94831d3bb24b85b9e0c31383bba9554a0c174
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-12-31 14:02:17 +0530

    Time: 246 ms (29.56%), Space: 22.7 MB (41.50%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,28 +1,16 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         n=len(prices)
-        nxt=[0]*(2*k+1)
-        for ind in range(n-1,-1,-1):
-            cur=[0]*(2*k+1)
-            for trans in range(2*k-1,-1,-1):
-                if trans%2==0:
-                    profit=max(-prices[ind]+nxt[trans+1],nxt[trans])
-                else:
-                    profit=max(prices[ind]+nxt[trans+1],nxt[trans])
-                cur[trans]=profit
-            nxt=cur
-        return nxt[0]
-#         def solve(ind,trans):
-#             if ind==len(prices) or trans==2*k:
-#                 return 0
-#             if dp[ind][trans]!=-1:
-#                 return dp[ind][trans]
-#             if trans%2==0:
-#                 profit=max(-prices[ind]+solve(ind+1,trans+1),solve(ind+1,trans))
-#             else:
-                
-#                            profit=max(prices[ind]+solve(ind+1,trans+1),solve(ind+1,trans))
-#             dp[ind][trans]=profit
-#             return profit
-#         return solve(0,0)
-        
+        dp=[[float('-inf')]*(2*k) for i in range(n)]
+        def solve(ind,buy):
+            if ind>=len(prices) or buy==2*k:
+                return 0
+            if dp[ind][buy]!=float('-inf'):
+                return dp[ind][buy]
+            if buy%2==0:
+                dp[ind][buy]=max(-prices[ind]+solve(ind+1,buy+1),solve(ind+1,buy))
+                return dp[ind][buy]
+            else:
+                dp[ind][buy]=max(prices[ind]+solve(ind+1,buy+1),solve(ind+1,buy))
+                return dp[ind][buy]
+        return solve(0,0)
