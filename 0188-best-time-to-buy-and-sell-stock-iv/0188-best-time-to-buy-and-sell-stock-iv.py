34519190a5afa0276ce9203ec2074562267756e3
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-12-31 14:12:04 +0530

    Time: 122 ms (60.31%), Space: 21.8 MB (42.65%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         n=len(prices)
-        dp=[[float('-inf')]*(2*k) for i in range(n)]
-        def solve(ind,buy):
-            if ind>=len(prices) or buy==2*k:
-                return 0
-            if dp[ind][buy]!=float('-inf'):
-                return dp[ind][buy]
-            if buy%2==0:
-                dp[ind][buy]=max(-prices[ind]+solve(ind+1,buy+1),solve(ind+1,buy))
-                return dp[ind][buy]
-            else:
-                dp[ind][buy]=max(prices[ind]+solve(ind+1,buy+1),solve(ind+1,buy))
-                return dp[ind][buy]
-        return solve(0,0)
+        dp=[[float('-inf')]*(2*k+1) for i in range(n+1)]
+        for i in range(n+1):
+            dp[i][2*k]=0
+        for i in range(2*k+1):
+            dp[n][i]=0
+        for ind in range(n-1,-1,-1):
+            for buy in range(2*k-1,-1,-1):
+                if buy%2==0:
+                    dp[ind][buy]=max(-prices[ind]+dp[ind+1][buy+1],dp[ind+1][buy])
+                else:
+                    dp[ind][buy]=max(prices[ind]+dp[ind+1][buy+1],dp[ind+1][buy])
+        return dp[0][0]
