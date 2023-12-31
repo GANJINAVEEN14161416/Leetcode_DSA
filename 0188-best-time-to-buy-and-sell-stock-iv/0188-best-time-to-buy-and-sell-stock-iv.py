7b4cb0c9e8fa1cf0e6843ddef029ea02287cb9f3
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2023-12-31 14:17:06 +0530

    Time: 194 ms (36.26%), Space: 19.5 MB (50.07%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.py
@@ -2,14 +2,13 @@ class Solution:
     def maxProfit(self, k: int, prices: List[int]) -> int:
         n=len(prices)
         dp=[[float('-inf')]*(2*k+1) for i in range(n+1)]
-        for i in range(n+1):
-            dp[i][2*k]=0
-        for i in range(2*k+1):
-            dp[n][i]=0
+        nxt=[0]*(2*k+1)
         for ind in range(n-1,-1,-1):
+            cur=[0]*(2*k+1)
             for buy in range(2*k-1,-1,-1):
                 if buy%2==0:
-                    dp[ind][buy]=max(-prices[ind]+dp[ind+1][buy+1],dp[ind+1][buy])
+                    cur[buy]=max(-prices[ind]+nxt[buy+1],nxt[buy])
                 else:
-                    dp[ind][buy]=max(prices[ind]+dp[ind+1][buy+1],dp[ind+1][buy])
-        return dp[0][0]
+                    cur[buy]=max(prices[ind]+nxt[buy+1],nxt[buy])
+            nxt=cur
+        return cur[0]
