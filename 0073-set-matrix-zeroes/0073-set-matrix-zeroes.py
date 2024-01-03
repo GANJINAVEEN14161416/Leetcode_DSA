3e21c79154af6259f3e769ecc624c12a9df0c196
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2024-01-03 08:50:39 +0530

    Time: 215 ms (5.03%), Space: 18 MB (22.44%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -1,30 +1,20 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
-        m=len(matrix)
-        n=len(matrix[0])
-        iszerorow=False
-        iszerocol=False
-        for i in range(n):# for first row
-            if matrix[0][i]==0:
-                iszerorow=True
-        for i in range(m):# for first column
-            if matrix[i][0]==0:
-                iszerocol=True
-        for i in range(1,m):
-            for j in range(1,n):
-                if matrix[i][j]==0:
-                    matrix[0][j]=0# make first row zero
-                    matrix[i][0]=0 # make first column zero
-        for i in range(1,m):
-            for j in range(1,n):
-                if matrix[i][0]==0 or matrix[0][j]==0:# now make zero on respective row and column
-                    matrix[i][j]=0
-        if iszerorow==True:
-            for i in range(n): # make for first row
-                matrix[0][i]=0
-        if iszerocol==True:
-            for i in range(m):# make for first column
-                matrix[i][0]=0
-                
-            
+        q = deque()
+        n = len(matrix)
+        m = len(matrix[0])
+        for i in range(n):
+            for j in range(m):
+                if matrix[i][j] == 0:
+                    q.append((i, j))
+        four = [(-1, 0), (0, 1), (0, -1), (1, 0)]
+        while q:
+            x, y = q.popleft()
+            for i in range(4):
+                newrow = x + four[i][0]
+                newcol = y + four[i][1]
+                while newrow < n and newrow >= 0 and newcol < m and newcol >= 0:
+                    matrix[newrow][newcol] = 0
+                    newrow += four[i][0]
+                    newcol += four[i][1]
         
