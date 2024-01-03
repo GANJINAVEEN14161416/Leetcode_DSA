a873087ec9b740852ce1738675dbf95559f4d099
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2024-01-03 13:02:09 +0530

    Time: 121 ms (63.60%), Space: 20.8 MB (19.35%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -1,27 +1,40 @@
 class Solution:
     def calculate(self, s: str) -> int:
-        inner, outer, result, opt = 0, 0, 0, '+'
-        for i in range(len(s) + 1):
-            if i < len(s):
-                c = s[i]
-            else:
-                c = '+'
-
-            if c == ' ': 
-                continue
-            if c.isdigit():
-                inner = 10 * inner + int(c)
-                continue
-
-            if opt == '+':
-                result += outer
-                outer = inner
-            elif opt == '-':
-                result += outer
-                outer = -inner
-            elif opt == '*':
-                outer = outer * inner
-            elif opt == '/':
-                outer = int(outer / inner)
-            inner, opt = 0, c
-        return result + outer
+        def update(sign,value):
+            if sign=="+":
+                stack.append(value)
+            elif sign=="-":
+                stack.append(-value)
+            elif sign=="*":
+                stack.append(stack.pop()*value)
+            elif sign=="/":
+                stack.append(int(stack.pop()/value))
+        i=0
+        stack=[]
+        num=0
+        n=len(s)
+        sign="+"
+        while i<n:
+            #print(stack)
+            if s[i].isdigit():
+                num=num*10+int(s[i])
+            elif s[i] in "+/*-":
+                update(sign,num)
+                sign=s[i]
+                num=0
+            elif s[i]=="(":
+                num,j=self.calculate(s[i+1:])
+                i=j+i
+            elif s[i]==")":
+                update(sign,num)
+                return sum(stack),i+1
+            i+=1
+        update(sign,num)
+        mod=2**31-1
+        ans=sum(stack)
+        if ans<-2**31:
+            return ans%(-2**31)
+        if ans>mod:
+            return ans%(mod)
+        return ans
+        
