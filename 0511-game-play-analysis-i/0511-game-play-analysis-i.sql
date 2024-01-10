1c9abc46704854e985bb46d2bb567e54d63ffbab
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2024-01-10 20:23:52 +0530

    Time: 895 ms (54.49%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,2 +1,1 @@
-select player_id ,event_date as first_login from (select player_id,event_date,row_number() over (partition by player_id order by event_date) as row_num from activity ) as t
-where row_num=1
+select distinct player_id,first_value(event_date) over (partition by player_id order by event_date) as first_login from activity 
