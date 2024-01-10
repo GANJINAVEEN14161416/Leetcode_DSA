32dc9c7c9c9c5d4a66f0e6a1921adbb288f7a57d
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2024-01-10 20:07:22 +0530

    Time: 1800 ms (5.00%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,1 +1,6 @@
-select player_id,min(event_date) as first_login from activity group by player_id
+SELECT Player_Id ,Event_Date AS first_login from 
+(
+    SELECT Player_id,Event_Date,ROW_NUMBER() OVER(PARTITION BY player_id order by event_date) as Row_No
+    from activity
+)as t
+where Row_No=1;
