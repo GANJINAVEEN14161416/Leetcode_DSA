9deea0183e8c571cdad40dff8d81706ae00dec14
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2024-01-22 11:01:41 +0530

    Time: 86 ms (35.10%), Space: 18.7 MB (57.22%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.py
+++ 0137-single-number-ii/0137-single-number-ii.py
@@ -2,13 +2,12 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         ans=0
         for i in range(32):
-            temp=0
             x=1<<i
-            for k in range(len(nums)):
-                if nums[k]&x:
+            temp=0
+            for j in range(len(nums)):
+                if nums[j]&x:
                     temp+=1
             if temp%3!=0:
                 ans=ans|x
-                #print(ans)
         return ans if ans < (1<<31) else ans-(1<<32)
         
