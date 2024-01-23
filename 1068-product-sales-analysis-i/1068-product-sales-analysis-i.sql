b163b11c03ec2cd3d37a82a5d04369cf4f60ad4b
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2024-01-23 11:39:23 +0530

    Time: 2199 ms (44.08%), Space: 0B (100.00%) - LeetHub

--- 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
+++ 1068-product-sales-analysis-i/1068-product-sales-analysis-i.sql
@@ -1,2 +1,1 @@
-select y.product_name,x.year,x.price from sales x
-left join product y on x.product_id=y.product_id
+select y.product_name,x.year,x.price from sales x left join product y on x.product_id=y.product_id
