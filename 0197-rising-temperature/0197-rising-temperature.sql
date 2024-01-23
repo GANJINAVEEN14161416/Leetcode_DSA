1029fe6cc9eb4fdd982b7a869be328e76cdaee4b
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2024-01-23 12:23:06 +0530

    Time: 969 ms (35.07%), Space: 0B (100.00%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,2 +1,1 @@
-select x.id from weather x,weather y
-where x.temperature >y.temperature and datediff(x.recorddate,y.recorddate)=1
+select x.id from weather x ,weather y where y.temperature<x.temperature and datediff(x.recorddate,y.recorddate)=1;
