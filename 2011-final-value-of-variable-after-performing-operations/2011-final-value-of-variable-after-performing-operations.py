59dc9e6ef9b5f6a36e3dfdfc1a2c6bc828ac7d48
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2024-01-25 12:33:01 +0530

    Time: 51 ms (85.55%), Space: 16.7 MB (52.13%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -2,7 +2,7 @@ class Solution:
     def finalValueAfterOperations(self, operations: List[str]) -> int:
         count=0
         for i in operations:
-            if "++" in i:
+            if "++"==i[1:] or "++"==i[:-1]:
                 count+=1
             else:
                 count-=1
