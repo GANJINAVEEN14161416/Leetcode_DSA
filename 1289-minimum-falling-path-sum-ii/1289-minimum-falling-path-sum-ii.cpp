def99e7f1666dde42d9d1765a19a829f3d777062
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2024-04-27 17:21:39 +0530

    Time: 228 ms (49.80%), Space: 18.3 MB (34.63%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    int n;
-    int solve(int row,int col,vector<vector<int>>&grid,vector<vector<int>>&dp){
-        if(row==n)return 0;
-        int l=INT_MAX;
-        if(dp[row][col]!=1e9)return dp[row][col];
-        for(int newcol=0;newcol<n;newcol++){
-            if(newcol!=col){
-                l=min(l,solve(row+1,newcol,grid,dp)+grid[row][col]);
-            }
-        }
-        return dp[row][col]=l;
-    }
     int minFallingPathSum(vector<vector<int>>& grid) {
-        n=grid.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,1e9));
-        if(n==1)return grid[0][0];
-        int ans=INT_MAX;
-        for(int col=0;col<n;col++){
-            ans=min(ans,solve(0,col,grid,dp));
+        int n=grid.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i=0;i<n;i++){
+            dp[n-1][i]=grid[n-1][i];
         }
+        int ans=INT_MAX;
+        for(int row=n-2;row>=0;row--){
+            for(int col=0;col<n;col++){
+                int l=INT_MAX;
+                for(int newcol=0;newcol<n;newcol++){
+                    if(newcol!=col){
+                       l=min(l,grid[row][col]+dp[row+1][newcol]);
+                    }
+                }
+                dp[row][col]=l;
+            }
+        }   
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         cout<<dp[i][j]<<" ";
+        //     }
+        //     cout<<endl;
+        // }
+     for(int i=0;i<n;i++){
+         ans=min(ans,dp[0][i]);
+     }
         return ans;
     }
 };
