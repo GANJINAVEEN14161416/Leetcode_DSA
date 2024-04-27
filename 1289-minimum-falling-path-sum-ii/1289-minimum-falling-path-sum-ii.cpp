1e0d3be19d973c3ed1d75b7b05b939d662902a5d
Ganji Naveen <127024619+GANJINAVEEN14161416@users.noreply.github.com>
2024-04-27 18:12:46 +0530

    Time: 27 ms (86.19%), Space: 18.1 MB (57.39%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -2,31 +2,47 @@ class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& grid) {
         int n=grid.size();
+        int minValue1=-1;
+        int minValue2=-1;
         vector<vector<int>>dp(n+1,vector<int>(n+1,0));
         for(int i=0;i<n;i++){
             dp[n-1][i]=grid[n-1][i];
-        }
-        int ans=INT_MAX;
-        for(int row=n-2;row>=0;row--){
-            for(int col=0;col<n;col++){
-                int l=INT_MAX;
-                for(int newcol=0;newcol<n;newcol++){
-                    if(newcol!=col){
-                       l=min(l,grid[row][col]+dp[row+1][newcol]);
-                    }
-                }
-                dp[row][col]=l;
+            if(minValue1==-1 or dp[n-1][i]<=dp[n-1][minValue1]){
+                minValue2=minValue1;
+                minValue1=i;
             }
-        }   
+            else if(minValue2==-1 or dp[n-1][i]<=dp[n-1][minValue2]){
+                minValue2=i;
+            }
+        }
+        //cout<<minValue1<<" "<<minValue2;
+        for(int row=n-2;row>=0;row--) {
+               int temp1=1e9;
+                int temp2=1e9;
+            for(int col=0;col<n;col++){
+                if(minValue1!=col){
+                    dp[row][col]=grid[row][col]+dp[row+1][minValue1];
+                }
+                else if(minValue2!=col){
+                    dp[row][col]=grid[row][col]+dp[row+1][minValue2];
+                }
+                if(temp1==1e9 or dp[row][col]<=dp[row][temp1]){
+                    temp2=temp1;
+                    temp1=col;
+                }
+                else if(temp2==1e9 or dp[row][col]<=dp[row][temp2]){
+                    temp2=col;
+                }
+            }
+            minValue1=temp1;
+            minValue2=temp2;
+        }
         // for(int i=0;i<n;i++){
         //     for(int j=0;j<n;j++){
         //         cout<<dp[i][j]<<" ";
         //     }
         //     cout<<endl;
         // }
-     for(int i=0;i<n;i++){
-         ans=min(ans,dp[0][i]);
-     }
-        return ans;
+     return dp[0][minValue1];
     }
 };
